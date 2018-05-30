40de3ab40a336dcea0c466e6f659d6d5b5b173f0
shadow1512 <viacheslav.borisov@gmail.com>
2018-05-30 16:09:05 +0300

    поменяли домен на ip

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -6,7 +6,7 @@
  * Time: 15:20
  */
 
-$ch = curl_init('http://aptest/Test/EseddApi/Authenticate/GetToken/');
+$ch = curl_init('http://172.16.0.76/Test/EseddApi/Authenticate/GetToken/');
 curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 curl_setopt($ch, CURLOPT_USERPWD, "integra:Att3r0D0min4tu5");
@@ -15,6 +15,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 var_dump($status_code);
+var_dump($res);
 curl_close($ch);
 
 ?>

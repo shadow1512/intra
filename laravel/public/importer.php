c7344c392b1cc01c99938a0065e1f0af4ac7f450
shadow1512 <viacheslav.borisov@gmail.com>
2018-05-30 16:39:57 +0300

    Выделяем токен

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -6,6 +6,8 @@
  * Time: 15:20
  */
 
+$tok = null;
+
 $ch = curl_init('http://172.16.0.76/Test/EseddApi/Authenticate/GetToken/');
 curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
@@ -17,9 +19,17 @@ $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 if($status_code == 200) {
     $header_size = curl_getinfo($ch,CURLINFO_HEADER_SIZE);
     $header = substr($res, 0, $header_size);
-    var_dump($header);
+    $headers = explode("\r\n\r\n", $header);
+    foreach($headers as $parts) {
+        $part = explode(":", $parts);
+        if($part[0] == "Token") {
+            $tok = trim($part[1]);
+        }
+    }
 }
 
 curl_close($ch);
+
+var_dump($tok);
 
 ?>

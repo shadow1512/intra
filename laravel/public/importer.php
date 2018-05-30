e99c33b727c9c76541b35a544f9dcc8a8d4cec01
shadow1512 <viacheslav.borisov@gmail.com>
2018-05-30 16:26:27 +0300

    получаем токен

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -14,8 +14,12 @@ curl_setopt($ch, CURLOPT_USERPWD, 'integra:Att3r0D0min4tu5');
 //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-var_dump($status_code);
-var_dump($res);
+if($status_code == 200) {
+    $header_size = curl_getinfo($ch,CURLINFO_HEADER_SIZE);
+    $header = substr($res, 0, $header_size);
+    var_dump($header);
+}
+
 curl_close($ch);
 
 ?>

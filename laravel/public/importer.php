b3144fd57101d70c4bacd3ae36721b7b4c459029
shadow1512 <viacheslav.borisov@gmail.com>
2018-05-30 16:16:33 +0300

    еще одна комбинация заголовков

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -7,11 +7,11 @@
  */
 
 $ch = curl_init('http://172.16.0.76/Test/EseddApi/Authenticate/GetToken/');
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-curl_setopt($ch, CURLOPT_USERPWD, "integra:Att3r0D0min4tu5");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, 'integra:Att3r0D0min4tu5');
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 var_dump($status_code);

9ea126d3a7708641f90cfc6b8202759785875bfd
shadow1512 <viacheslav.borisov@gmail.com>
2018-05-30 16:31:59 +0300

    результат в переменную

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -10,7 +10,7 @@ $ch = curl_init('http://172.16.0.76/Test/EseddApi/Authenticate/GetToken/');
 curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($ch, CURLOPT_USERPWD, 'integra:Att3r0D0min4tu5');
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);

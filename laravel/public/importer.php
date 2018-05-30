cd88ec0112d1756f164affaf68e6cc2bbf5d2866
shadow1512 <viacheslav.borisov@gmail.com>
2018-05-30 15:55:48 +0300

    заголовки при авторизации

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -7,11 +7,11 @@
  */
 
 $ch = curl_init('http://aptest/Test/EseddApi/Authenticate/GetToken/');
+curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 curl_setopt($ch, CURLOPT_USERPWD, "integra:Att3r0D0min4tu5");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HEADER, false);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 var_dump($status_code);

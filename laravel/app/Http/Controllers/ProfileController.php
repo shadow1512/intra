5cf9fc8d2aeb52578dda5dba589fd502dec7561d
shadow1512 <viacheslav.borisov@gmail.com>
2018-06-12 19:33:58 +0300

    Ищем метод

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -72,8 +72,8 @@ class ProfileController extends Controller
     public function updateavatar(Request $request)
     {
         $path = Storage::disk('public')->putFile(Config::get('image.avatar_path'), $request->file('input_avatar'), 'public');
-        var_dump(Storage::disk('public')->size($path));
-        var_dump(Storage::disk('public')->mime_type($path));
+        var_dump(Storage::disk('public')->getSize($path));
+        var_dump(Storage::disk('public')->getMimetype($path));
 
     }
 }

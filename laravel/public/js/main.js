deea88fadcb09ec1450304ee8adf1dfac371f4c4
shadow1512 <viacheslav.borisov@gmail.com>
2018-06-13 02:20:25 +0300

    привязываем событие к динамическим элементам

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -80,10 +80,12 @@ $(document).ready(function(){
     popUp('.__js-modal-bill-lk', '.__js-modal-bill');
     popUp('.__js-modal-camera-lk', '.__js-modal-camera');
     popUp('.__js-modal-profile-lk', '.__js-modal-profile');
-    popUp('.order_calendar_cnt_add', '.__js-modal-order', function(but, win) {
+    popUp('.order_calendar_cnt_add', '.__js-modal-order');
+
+    /*function(but, win) {
         var dd = $(but).parent().find("span.order_date").text();
         $(win).find("input[name='input_date']").val(dd);
-    });
+    }*/
 
     //eo modal window
 
--- laravel/public/js/room.js
+++ laravel/public/js/room.js
@@ -2,5 +2,10 @@ $("#input_time_start,#input_time_end").datetimepicker({
     lang:'ru',
     datepicker:false,
     timepicker:true,
-    format:'H:i'
+    format:'H:i',
+    step: 5,
+});
+
+$(document).on("click", "#input_time_start,#input_time_end", function() {
+   $(this).datetimepicker('toggle');
 });

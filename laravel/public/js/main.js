eb61551ad290a6aff19031781dd2e0b580232787
shadow1512 <viacheslav.borisov@gmail.com>
2018-06-13 02:35:58 +0300

    Умный пикер + нужный коэффициент показа

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -82,10 +82,10 @@ $(document).ready(function(){
     popUp('.__js-modal-profile-lk', '.__js-modal-profile');
     popUp('.order_calendar_cnt_add', '.__js-modal-order');
 
-    /*function(but, win) {
+    function(but, win) {
         var dd = $(but).parent().find("span.order_date").text();
-        $(win).find("input[name='input_date']").val(dd);
-    }*/
+        $(win).find("input[name='input_date_booking']").val(dd);
+    }
 
     //eo modal window
 
--- laravel/public/js/room.js
+++ laravel/public/js/room.js
@@ -1,9 +1,27 @@
-$("#input_time_start,#input_time_end").datetimepicker({
+$("#input_time_start").datetimepicker({
     lang:'ru',
     datepicker:false,
     timepicker:true,
     format:'H:i',
     step: 5,
+    onShow:function( ct ){
+        this.setOptions({
+            maxTime:$('#input_time_end').val()?$('#input_time_end').val():false
+        })
+    },
+});
+
+$("#input_time_end").datetimepicker({
+    lang:'ru',
+    datepicker:false,
+    timepicker:true,
+    format:'H:i',
+    step: 5,
+    onShow:function( ct ){
+        this.setOptions({
+            minTime:$('#input_time_start').val()?$('#input_time_start').val():false
+        })
+    },
 });
 
 $(document).on("click", "#input_time_start,#input_time_end", function() {

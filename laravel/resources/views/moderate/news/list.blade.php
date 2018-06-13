bae8f2f507673c91b7896e2a06abaf0aeba98866
shadow1512 <viacheslav.borisov@gmail.com>
2018-06-13 19:30:41 +0300

    В списке правильная дата

--- laravel/resources/views/moderate/news/list.blade.php
+++ laravel/resources/views/moderate/news/list.blade.php
@@ -18,7 +18,7 @@
             @if (count($news))
                 @foreach($news as $item)
             <div class="row">
-                <div class="col-md-6"> {{ $item->title }}</div>
+                <div class="col-md-6">({{ date("d.m.Y", strtotime($news->published_at)) }}) {{ $item->title }}</div>
                 <div class="col-md-3"><a href="{{ route('moderate.news.edit', ["id" => $item->id]) }}"><span class="glyphicon glyphicon-edit"></span></a></div>
                 <div class="col-md-3"><form method="POST" action="{{ route('moderate.news.delete', ["id" => $item->id]) }}">{{ method_field('DELETE') }}{{ csrf_field() }}<a href="" class="deleteRecord"><span class="glyphicon glyphicon-remove-sign"></span></a></form></div>
             </div>

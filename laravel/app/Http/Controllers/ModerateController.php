cb64a4cacdfb9166f4f6d64ed1cb0513ed935772
shadow1512 <viacheslav.borisov@gmail.com>
2018-06-14 02:55:36 +0300

    дебаг занесения данных о разделе книги

--- laravel/app/Http/Controllers/ModerateController.php
+++ laravel/app/Http/Controllers/ModerateController.php
@@ -334,6 +334,7 @@ class ModerateController extends Controller
         $book->save();
 
         DB::table('lib_books_razdels')->where("book_id", "=", $id)->delete();
+        var_dump($request->input('razdels[]'));exit();
         if(count($request->input('razdels[]'))) {
             foreach($request->input('razdels[]') as $razdel_id) {
                 DB::table('lib_books_razdels')->insert(

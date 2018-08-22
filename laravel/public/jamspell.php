94026bec1ad571ac73dbee1a3a5f7c091ad08dfe
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-22 20:52:45 +0300

    разные url

--- laravel/public/jamspell.php
+++ laravel/public/jamspell.php
@@ -9,16 +9,19 @@
 //exec("curl -d \"ствкан\" http://localhost:8080/candidates", $out);
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://localhost/candidates');
+$url = "fix";
+if(isset($argv[1])) {
+    $url = $argv[1];
+}
+$field = "дериво";
+if(isset($argv[2])) {
+    $field = $argv[2];
+}
+curl_setopt($ch, CURLOPT_URL, 'http://localhost/' . $url);
 curl_setopt($ch, CURLOPT_PORT, 8080);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
-
-$field = "дериво";
-if(isset($argv[1])) {
-    $field = $argv[1];
-}
 curl_setopt($ch, CURLOPT_POSTFIELDS, array("text"=>$field));
 $res = curl_exec($ch);
 

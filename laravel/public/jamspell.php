ce662609f380c2d8064d2b74c29f599aa84a6e8e
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-22 20:05:04 +0300

    вставка трансфер

--- laravel/public/jamspell.php
+++ laravel/public/jamspell.php
@@ -8,6 +8,7 @@
 $ch = curl_init('http://localhost:8080/candidates');
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 if($status_code == 200) {

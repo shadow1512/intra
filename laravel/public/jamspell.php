9ce0e96ceec528354276c4c7c6e9a66b59c7f27b
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-22 19:47:08 +0300

    поправили опцию

--- laravel/public/jamspell.php
+++ laravel/public/jamspell.php
@@ -7,7 +7,7 @@
  */
 $ch = curl_init('http://127.0.0.1/candidates');
 curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_POST);
+curl_setopt($ch, CURLOPT_POST, true);
 $res = curl_exec($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 if($status_code == 200) {

6346ad779e737524242aa7bd1498a97ca0e415aa
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-22 20:21:25 +0300

    порт альтернативно

--- laravel/public/jamspell.php
+++ laravel/public/jamspell.php
@@ -6,18 +6,14 @@
  * Time: 15:20
  */
 
-exec("curl -d \"ствкан\" http://localhost:8080/candidates", $out);
+//exec("curl -d \"ствкан\" http://localhost:8080/candidates", $out);
 
-/*$ch = curl_init('http://localhost:8080/candidates');
-curl_setopt($ch, CURLOPT_HEADER, 0);
+$ch = curl_init('http://localhost/candidates');
+curl_setopt($ch, CURLOPT_PORT, 8080);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $res = curl_exec($ch);
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-if($status_code == 200) {
-    var_dump($res);
-}
-
-curl_close($ch);*/
-var_dump($out);
+var_dump($res);
+curl_close($ch);
+//var_dump($out);
 ?>

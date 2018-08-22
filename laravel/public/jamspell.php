49e1556b887f21392d93118bf9ddf5fd4f9d0ea8
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-22 20:25:55 +0300

    пост-поля

--- laravel/public/jamspell.php
+++ laravel/public/jamspell.php
@@ -8,11 +8,14 @@
 
 //exec("curl -d \"ствкан\" http://localhost:8080/candidates", $out);
 
-$ch = curl_init('http://localhost/candidates');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://localhost/candidates');
 curl_setopt($ch, CURLOPT_PORT, 8080);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'дериво');
 $res = curl_exec($ch);
+
 var_dump($res);
 curl_close($ch);
 //var_dump($out);

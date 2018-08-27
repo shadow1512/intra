60d412c95cd312230067202d621474dea5367540
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-27 14:08:52 +0300

    убрал заглавные буквы в кодировке

--- laravel/app/Http/Controllers/IndexerController.php
+++ laravel/app/Http/Controllers/IndexerController.php
@@ -37,7 +37,7 @@ class IndexerController extends Controller
         Terms::truncate();
         //создаем файлик, в который потом добавим в словарь
         // Setup the personal dictionary
-        $pspell_config = pspell_config_create("ru", "", "", "UTF-8");
+        $pspell_config = pspell_config_create("ru", "", "", "utf-8");
         pspell_config_mode($pspell_config,  PSPELL_NORMAL);
         pspell_config_personal($pspell_config, storage_path('app/public/dict/pspell_custom.rws'));
         $pspell_link = pspell_new_config($pspell_config);

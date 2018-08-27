53c270e53c619ceb9119282e98bb1c61dde23ba4
shadow1512 <viacheslav.borisov@gmail.com>
2018-08-27 12:00:33 +0300

    модифицировали метод доступа к записываемому файлу

--- laravel/app/Http/Controllers/IndexerController.php
+++ laravel/app/Http/Controllers/IndexerController.php
@@ -35,7 +35,7 @@ class IndexerController extends Controller
         //
         Terms::truncate();
         //создаем файлик, который потом добавим в словарь
-        Storage::put('pspell_custom.txt', "", 'public');
+        Storage::disk('public')->put('pspell_custom.txt', "", 'public');
         //Секция "пользователи"
         $users = User::orderBy('name', 'asc')
             ->leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
@@ -53,7 +53,7 @@ class IndexerController extends Controller
             $term->record = $user->id;
             $term->save();
 
-            Storage::append('pspell_custom.txt', $user->fname);
+            Storage::disk('public')->append('pspell_custom.txt', $user->fname);
             //Фамилия
 
             $term = new Terms();
@@ -66,7 +66,7 @@ class IndexerController extends Controller
             $term->record = $user->id;
             $term->save();
 
-            Storage::append('pspell_custom.txt', $user->lname);
+            Storage::disk('public')->append('pspell_custom.txt', $user->lname);
             //Отчество
 
             $term = new Terms();
@@ -79,7 +79,7 @@ class IndexerController extends Controller
             $term->record = $user->id;
             $term->save();
 
-            Storage::append('pspell_custom.txt', $user->mname);
+            Storage::disk('public')->append('pspell_custom.txt', $user->mname);
             //Номер комнаты
 
             if(trim($user->room)) {

13f98394541a4b18825aa1464ceed4e5cdad839a
shadow1512 <viacheslav.borisov@gmail.com>
2018-09-07 03:03:18 +0300

    орфографическая ошибка

--- laravel/app/Http/Controllers/IndexerController.php
+++ laravel/app/Http/Controllers/IndexerController.php
@@ -303,7 +303,7 @@ class IndexerController extends Controller
         $xmlstring  =   Storage::disk('public')->get('/xml/sprav.xml');
         $olddict    =   simplexml_load_string($xmlstring);
         if(($olddict->count() > 0) &&  ($olddict->children()->count()   >   0)){
-            $items  =   $olddict->children()->cildren();
+            $items  =   $olddict->children()->children();
             foreach($items as $item) {
                 var_dump($item);exit();
                 if(isset($item["phones"]["phone"]) && count($item["phones"]["phone"])) {

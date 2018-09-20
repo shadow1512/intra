03d44a0f14d0bd9142487a0487140f81746f2d46
shadow1512 <viacheslav.borisov@gmail.com>
2018-09-20 17:32:03 +0300

    скрытие/раскрытие поиска)

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -157,6 +157,7 @@ class UserController extends Controller
 
         $hide_search_form = $request->cookie('hide_search_form');
 
+        var_dump($hide_search_form);
 
         return view('users.search', [   "crumbs"            =>  $crumbs,
                                         "directory_name"    =>  $directory_name,

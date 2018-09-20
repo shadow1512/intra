f33e5fcd1befdb431d11ed4d7f850ff104dcc41a
shadow1512 <viacheslav.borisov@gmail.com>
2018-09-21 02:35:37 +0300

    похоже про кодировку

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -65,8 +65,7 @@ class HomeController extends Controller
         $res = curl_exec($ch);
         $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $kitchen_menu   =   "";
-
-        var_dump($res);
+        $res=   ("windows-1251",    "utf-8",   $res);
         if($status_code == 200) {
             preg_match("/<body[^>]*>(.*?)<\/body>/ius", $res, $matches);
             if(count($matches)) {

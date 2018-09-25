81ae21be02951e2851262fc4cf8c7da7a7cc8173
shadow1512 <viacheslav.borisov@gmail.com>
2018-09-26 02:09:40 +0300

    комнаты и контакты вынесли в конструктор в контроллере обслуживания

--- laravel/app/Http/Controllers/ServicesController.php
+++ laravel/app/Http/Controllers/ServicesController.php
@@ -10,6 +10,9 @@ use App\Rooms;
 
 class ServicesController extends Controller
 {
+    public $rooms;
+
+    public $contacts;
     /**
      * Create a new controller instance.
      *
@@ -18,6 +21,14 @@ class ServicesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
+        //Комнаты
+        $this->rooms = Rooms::orderBy('name')->get();
+        //Контакты выбранные
+        if(Auth::check()) {
+            $this->contacts = User::select("users.id", "users.avatar", "users.fname", "users.lname", "users.mname", "users.position", "users.email", "users.phone")
+                ->leftJoin('user_contacts', 'user_contacts.contact_id', '=', 'users.id')->where('user_contacts.user_id', '=', Auth::user()->id)->get();
+        }
     }
 
     /**
@@ -27,19 +38,16 @@ class ServicesController extends Controller
      */
     public function teh()
     {
-        //Комнаты
-        $rooms = Rooms::orderBy('name')->get();
-
-        return view('services.teh', ["rooms"    =>  $rooms]);
+        return view('services.teh', ["rooms"    =>  $this->rooms, "contacts"  =>  $this->contacts]);
     }
 
     public function cartridge()
     {
-        return view('services.cartridge', ["rooms"    =>  $rooms]);
+        return view('services.cartridge', ["rooms"    =>  $this->rooms, "contacts"  =>  $this->contacts]);
     }
 
     public function mail()
     {
-        return view('services.mail', ["rooms"    =>  $rooms]);
+        return view('services.mail', ["rooms"    =>  $this->rooms, "contacts"  =>  $this->contacts]);
     }
 }

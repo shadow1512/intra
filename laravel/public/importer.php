6aa26b80e30ce0f9ea474df3bda9831b89399ce9
shadow1512 <viacheslav.borisov@gmail.com>
2018-10-11 19:18:11 +0300

    обновление без "новых" данных

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -167,9 +167,13 @@ if($tok) {
                             $status_code_data = curl_getinfo($chauthdata, CURLINFO_HTTP_CODE);
                             if($status_code_data == 200) {
                                 $obj_authdata = json_decode($resauthdata);
+                                var_dump($obj_authdata);exit();
                                 mysqli_query($conn, "INSERT INTO user_keys (`key`, `user_id`, `parent_key`,  `sid`,  `ad_deleted`, `user_login`) VALUES ('" . $obj->UID . "', $user_id, '" . $obj->Parent . "', '"  .   $obj_authdata->UserSID  .   "', "   .   $obj_authdata->Pruz .   ",  '"  .   $obj_authdata->Username .   "')");
                             }
                             else {
+                                var_dump($status_code_data);
+                                var_dump('http://172.16.0.76/Test/EseddApi/Access/GetUser?uid=' . $obj->UID);
+                                exit();
                                 mysqli_query($conn, "INSERT INTO user_keys (`key`, `user_id`, `parent_key`) VALUES ('" . $obj->UID . "', $user_id, '" . $obj->Parent . "')");
                                 print("error:no AD data for $user_id\r\n");
                             }

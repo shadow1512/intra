82169fbe122b638cda118fd42a5391fc323d7e18
shadow1512 <viacheslav.borisov@gmail.com>
2018-10-16 04:35:05 +0300

    Замена convertdate в нововстях

--- laravel/resources/views/news/fulllist.blade.php
+++ laravel/resources/views/news/fulllist.blade.php
@@ -7,7 +7,11 @@
     <ul class="news_ul">
         @foreach($news as $item)
         <li class="news_li __important"><a href="{{ route('news.item', ['id' => $item->id])}}" class="news_li_lk">{{ $item->title }}</a>
-            <div class="news_li_date">@convertdate($item->published_at)</div>
+            <div class="news_li_date">@php
+                    $months =   array("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря");
+                    $month  =   $months[date("n", strtotime($item->published_at))   -1];
+                    $day    =   date("j",   strtotime($item->published_at));
+                @endphp {{ $day }} {{ $month }}</div>
         </li>
         @endforeach
     @endif

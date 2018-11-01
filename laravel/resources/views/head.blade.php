b12671dad096a1d8bead8d72e626cd9987ce50cd
astreniya <astreniya@kodeks.ru>
2018-11-01 18:24:36 +0300

    FIX

--- laravel/resources/views/head.blade.php
+++ laravel/resources/views/head.blade.php
@@ -1,8 +1,8 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="/styles/css/styles.css">
-    <link rel="stylesheet" href="/styles/css/jquery.datetimepicker.css">
     <link rel="stylesheet" href="/styles/css/datepicker/jquery-ui-1.9.2.custom.css">
+    <link rel="stylesheet" href="/styles/css/jquery.datetimepicker.css">
+    <link rel="stylesheet" href="/styles/css/styles.css">
     <meta charset="UTF-8">
 </head>

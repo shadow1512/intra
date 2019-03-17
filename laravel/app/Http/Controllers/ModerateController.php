8363afe6184936deb0177fc2c8d96b4ec6c7a080
shadow1512 <viacheslav.borisov@gmail.com>
2019-03-18 02:51:21 +0300

    ошибка в переменной

--- laravel/app/Http/Controllers/ModerateController.php
+++ laravel/app/Http/Controllers/ModerateController.php
@@ -587,11 +587,11 @@ class ModerateController extends Controller
             $dp->dep_id         =   $value;
             $dp->work_title     =   $work;
 
-            $curDep     =   Dep::findOrFail($id);
+            $curDep     =   Dep::findOrFail($value);
             $cur_length =   mb_strlen($curDep["parent_id"], "UTF-8");
             $chef       =  $max - $cur_length;
             $dp->chef           =   $chef;
-        
+
             $dp->save();
         }
         return redirect(route('moderate.users.start'));

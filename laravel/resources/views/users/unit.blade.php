a880e2b62de2e979dfd1656815a82c444d3d2847
shadow1512 <viacheslav.borisov@gmail.com>
2019-03-20 02:39:04 +0300

    перенос удаления контакта в другую область

--- laravel/resources/views/users/unit.blade.php
+++ laravel/resources/views/users/unit.blade.php
@@ -30,8 +30,6 @@
             </div>
             @if(!in_array($user->id,    $contact_ids))
                 <a href="{{route('profile.addcontact', ['id' => $user->id])}}" class="btn profile_info_i_btn">Добавить в контакты</a>
-            @else
-                <a href="{{route('profile.deletecontact', ['id' => $user->id])}}" title="Удалить из избранного" class="directory_lst_i_action_lk"><svg class="directory_lst_i_action_del" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27.37559 27.45416"><g><path d="M0 26.11L26.033.1l1.343 1.344-26.033 26.01z"/><path d="M0 1.343L1.343 0l26.022 26.02-1.344 1.345z"/></g></svg></a></div>
             @endif
         </div>
     </div>

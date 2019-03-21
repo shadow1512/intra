603ddcf5b8fb1ecf31170bee5cb5727c5743d2e3
shadow1512 <viacheslav.borisov@gmail.com>
2019-03-21 22:20:39 +0300

    Исправлена ошибка в колонке

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -86,7 +86,7 @@ class UserController extends Controller
         if(!is_null($id)) {
             $currentDep     = Dep::findOrFail($id);
             $users = User::leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
-                ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef', 'deps_peoples.parent_id')
+                ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef', 'deps.parent_id')
                 ->leftJoin('deps', 'deps_peoples.dep_id', '=', 'deps.id')
                 ->whereRaw("deps_peoples.dep_id IN (SELECT id FROM deps WHERE parent_id LIKE '" . $currentDep->parent_id . "%')")
                 ->orderBy('LENGTH(parent_id)',  'asc')
@@ -96,7 +96,7 @@ class UserController extends Controller
         else {
             $users = User::leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
                 ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef')
-                ->leftJoin('deps', 'deps_peoples.dep_id', '=', 'deps.id')
+                ->leftJoin('deps', 'deps.dep_id', '=', 'deps.id')
                 ->orderBy('deps_peoples.chef', 'desc')->orderBy('LENGTH(parent_id)',  'asc')->orderBy('users.name', 'asc')
                 ->limit(100)->get();
         }

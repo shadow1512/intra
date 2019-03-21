fdcada1833502997421f65499dbcb921532523b7
shadow1512 <viacheslav.borisov@gmail.com>
2019-03-21 22:25:20 +0300

    Изменена команда сортировки

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -89,7 +89,7 @@ class UserController extends Controller
                 ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef', 'deps.parent_id')
                 ->leftJoin('deps', 'deps_peoples.dep_id', '=', 'deps.id')
                 ->whereRaw("deps_peoples.dep_id IN (SELECT id FROM deps WHERE parent_id LIKE '" . $currentDep->parent_id . "%')")
-                ->orderRaw('LENGTH(parent_id)',  'asc')
+                ->orderByRaw('LENGTH(parent_id)',  'asc')
                 ->orderBy('deps_peoples.chef', 'desc')->orderBy('users.name', 'asc')
                 ->limit(200)->get();
         }
@@ -97,7 +97,7 @@ class UserController extends Controller
             $users = User::leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
                 ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef')
                 ->leftJoin('deps', 'deps.dep_id', '=', 'deps.id')
-                ->orderBy('deps_peoples.chef', 'desc')->orderRaw('LENGTH(parent_id)',  'asc')->orderBy('users.name', 'asc')
+                ->orderBy('deps_peoples.chef', 'desc')->orderByRaw('LENGTH(parent_id)',  'asc')->orderBy('users.name', 'asc')
                 ->limit(100)->get();
         }
 

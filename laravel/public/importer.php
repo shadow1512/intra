c1f52983b8e1285c393e9a0599ebf0d9dc7cc8c6
shadow1512 <viacheslav.borisov@gmail.com>
2019-04-06 18:14:59 +0300

    доработана логика загрузки счетов сотрудников

--- laravel/public/importer.php
+++ laravel/public/importer.php
@@ -626,11 +626,14 @@ function deleteTest($conn) {
 }
 
 function getDinnerBills($conn) {
+    $total_inserted =   0;
+
     $ch = curl_init('http://intra.lan.kodeks.net/cooking/');
     curl_setopt($ch, CURLINFO_HEADER_OUT, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $res = curl_exec($ch);
     $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
     if($status_code == 200) {
         /*Внимание!! Тут отключается вывод ошибок в буфер и ошибки передаются на обработку в скрипты.
         Связано это с тем, что он ругается на <br>, например, а такие теги присутствуют в получаемом документе*/
@@ -640,13 +643,102 @@ function getDinnerBills($conn) {
         preg_match("/<body[^>]*>(.*?)<\/body>/ius", $res, $matches);
         if (count($matches)) {
             $doc->loadHTML("<?xml encoding=\"utf-8\" ?><body>"   .   $matches[1] .   "</body>");
-            var_dump($doc);
             if($doc) {
                 $deps  =   $doc->getElementsByTagName("a");
                 if($deps   &&  ($deps->count() >   0)) {
                     foreach($deps   as $dep) {
-                        var_dump($dep->textContent);
-                        var_dump($dep->getAttribute('href'));
+                        $linkdep = mysqli_query($conn, "SELECT id FROM deps WHERE name LIKE '" . $dep->textContent . "%' AND LENGTH(parent_id)=" . CODE_LENGTH .   " LIMIT 1");
+                        if ($linkdep    &&  $linkdep->num_rows  >   0) {
+                            $linkRow = $linkdep->fetch_assoc();
+
+                            $ch = curl_init('http://intra.lan.kodeks.net/cooking/' . $dep->getAttribute('href'));
+                            curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+                            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                            $res_users = curl_exec($ch);
+                            $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                            curl_close($ch);
+                            if ($status_code == 200) {
+                                $res_users = iconv("windows-1251", "utf-8", $res_users);
+                                $doc_users = new DomDocument('1.0', 'utf-8');
+                                preg_match("/<body[^>]*>(.*?)<\/body>/ius", $res_users, $matches_users);
+                                if (count($matches_users)) {
+                                    $doc_users->loadHTML("<?xml encoding=\"utf-8\" ?><body>" . $matches_users[1] . "</body>");
+                                    if ($doc_users) {
+                                        $users_and_bills = $doc_users->getElementsByTagName("td");
+                                        $numusers   =   $users_and_bills->count();
+                                        if ($users_and_bills && ($numusers > 0)) {
+                                            for($i  =   0;  $i< $numusers;  $i  =   $i+2) {
+                                                $user   =   $users_and_bills->item($i);
+
+                                                $names  =   preg_replace("/\s/ius",    " ", $user->textContent);
+                                                $lname = $fname = $mname = "";
+                                                $names = explode( " ", $names);
+                                                if (isset($names[0])) {
+                                                    $lname = preg_replace("/[^А-я]/ius",    "", $names[0]);
+                                                }
+                                                if (isset($names[1])) {
+                                                    $fname = preg_replace("/[^А-я]/ius",    "", $names[1]);
+                                                }
+                                                if (isset($names[2])) {
+                                                    $mname = preg_replace("/[^А-я]/ius",    "", $names[2]);
+                                                }
+                                                if ($lname && $fname) {
+                                                    $linkuser = mysqli_query($conn, "SELECT id FROM users LEFT JOIN deps_peoples ON (users.id  = deps_peoples.people_id) WHERE fname LIKE '" . $fname . "' AND lname LIKE '" . $lname . "' AND dep_id=" .   $linkRow["id"]);
+                                                    if ($linkuser    &&  $linkuser->num_rows  >   0) {
+                                                        $linkRowUser    =   null;
+                                                        if($linkuser->num_rows  >   1) {
+                                                            $linkuser = mysqli_query($conn, "SELECT id FROM users LEFT JOIN deps_peoples ON (users.id  = deps_peoples.people_id) WHERE fname LIKE '" . $fname . "' AND lname LIKE '" . $lname . "' AND mname LIKE '" . $mname . "' AND dep_id=" .   $linkRow["id"]);
+                                                            if ($linkuser    &&  $linkuser->num_rows  >   0) {
+                                                                $linkRowUser = $linkuser->fetch_assoc();
+                                                            }
+                                                            else {
+                                                                print("Не найдена связь для сотрудника (отчество!) "  .   $user->textContent   .   ". http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                                                            }
+                                                        }
+                                                        else {
+                                                            $linkRowUser = $linkuser->fetch_assoc();
+                                                        }
+
+                                                        if(!is_null($linkRowUser)) {
+                                                            $summ   =   (int)$users_and_bills->item($i+1)->textContent;
+                                                            $insres = mysqli_query($conn,
+                                                                "INSERT INTO users_dinner_bills (`user_id`, `date_created`, `summ`) VALUES ("   .   $linkRowUser["id"]  .   ", '"   .   date("Y-m-d")   .   "', $summ)");
+                                                            if(!$insres) {
+                                                                printf("Error: %s\n", mysqli_error($conn)   .   "\r\n");
+                                                            }
+                                                            else {
+                                                                $total_inserted ++;
+                                                            }
+                                                        }
+                                                    }
+                                                    else {
+                                                        print("Не найдена связь для сотрудника "  .   $user->textContent   .   ". http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                                                    }
+                                                }
+                                                else {
+                                                    print("Не выделены имя и фамилия для сотрудника:  "  .   $user->textContent   .   ". http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                                                }
+                                            }
+                                        }
+                                        else {
+                                            print("Пустой список сотрудников: http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                                        }
+                                    }
+                                    else {
+                                        print("Ошибка преобразования файла со списком сотрудников. Проблема со структурой: http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                                    }
+                                }
+                                else {
+                                    print("Нет тега body. Проблема со структурой: http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                                }
+                            }
+                            else {
+                                print("Ошибка получения файла со списком сотрудников: http://intra.lan.kodeks.net/cooking/"  .   $dep->getAttribute('href')   .   "\r\n");
+                            }
+                        }
+                        else {
+                            print("Не найдена связь для департамента "  .   $dep->textContent   .   ", список сотрудников игнорируется\r\n");
+                        }
                     }
                 }
                 else {
@@ -664,6 +756,8 @@ function getDinnerBills($conn) {
     else {
         print("Ошибка получения файла со списком департаментов\r\n");
     }
+
+    print("Загружено $total_inserted счетов\r\n");
 }
 
 function cleanStructData($conn) {

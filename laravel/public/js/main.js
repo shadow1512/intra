cecf5f198bde209c31d3a6fd81ec7125fb8100d5
shadow1512 <viacheslav.borisov@gmail.com>
2019-04-14 00:20:19 +0300

    отлаживаем таймер

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -176,8 +176,8 @@ popUp('.reserve_table_column_btn', '.__js-modal-order', function(but, win) {
             ],
             mask:true,
             onShow:function( ct ){
+                alert($(win).find('#input_time_end').val());
                 this.setOptions({
-                    alert($(win).find('#input_time_end').val());
                     maxTime:$(win).find('#input_time_end').val()?$(win).find('#input_time_end').val():'18:30'
                 });
             }

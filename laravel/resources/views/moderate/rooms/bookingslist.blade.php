518d52312da3c811d5e4c8bb76997557006e4bb9
shadow1512 <viacheslav.borisov@gmail.com>
2019-04-14 19:18:50 +0300

    небольшая поправка в верстке

--- laravel/resources/views/moderate/rooms/bookingslist.blade.php
+++ laravel/resources/views/moderate/rooms/bookingslist.blade.php
@@ -11,11 +11,11 @@
             @if (count($bookings))
             @foreach($bookings as $booking)
             <div class="row">
-                <div class="col-md-4">{{ $booking->name }}</div>
-                <div class="col-md-1">{{date("d.m.Y",   strtotime($booking->date_book))}}</div>
+                <div class="col-md-3">{{ $booking->name }}</div>
+                <div class="col-md-2">{{date("d.m.Y",   strtotime($booking->date_book))}}</div>
                 <div class="col-md-1">{{ $booking->time_start}}</div>
                 <div class="col-md-1">{{ $booking->time_end}}</div>
-                <div class="col-md-3">{{ $booking->lname }} {{mb_substr($booking->fname,    0,  1,  "UTF-8")}}.@if ($booking->mname) {{mb_substr($booking->mname,    0,  1,  "UTF-8")}}. @endif <br/>@if ($booking->phone) {{$booking->phone}}, @endif @if ($booking->email) {{$booking->email}} @endif</div>
+                <div class="col-md-3">{{ $booking->lname }} {{mb_substr($booking->fname,    0,  1,  "UTF-8")}}.@if ($booking->mname) {{mb_substr($booking->mname,    0,  1,  "UTF-8")}}. @endif <br/>@if ($booking->phone) {{$booking->phone}},<br/>@endif @if ($booking->email) {{$booking->email}} @endif</div>
                 <div class="col-md-2"><a href="{{ route('moderate.rooms.bookingconfirm', ["id" => $booking->id]) }}">Подтвердить</a></div>
             </div>
             @endforeach

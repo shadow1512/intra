6fd8df8c885d40ffb9ccb3b5b150ea072a9c0f1d
astreniya <astreniya@kodeks.ru>
2019-04-16 14:36:37 +0300

    https://task.kodeks.ru/task/issues/25027

--- laravel/resources/views/layouts/library.blade.php
+++ laravel/resources/views/layouts/library.blade.php
@@ -17,5 +17,11 @@
 <!--footer-->
 @include('footer')
 <!--eo footer-->
+<script src="/js/libs/jquery-3.1.0.js"></script>
+<script src="/js/libs/chosen.jquery.min.js"></script>
+<script src="/js/libs/owl.carousel.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/libs/jquery-ui-1.9.2.custom.min.js"></script>
+<script src="/js/libs/jquery.datepicker.extension.range.min.js"></script>
 </body>
 </html>

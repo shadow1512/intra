941b4c36cc51ad897f9fb157eee12ec8f3787c20
shadow1512 <viacheslav.borisov@gmail.com>
2019-05-30 00:15:26 +0300

    Не находился метод

--- laravel/app/Http/Controllers/NewsController.php
+++ laravel/app/Http/Controllers/NewsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\News;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 use DateTime;
 
 class NewsController extends Controller

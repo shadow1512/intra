bccf668a678e79545c79595015cbe3955a054ec6
shadow1512 <viacheslav.borisov@gmail.com>
2019-05-31 01:59:32 +0300

    немного поменяли метод submit

--- laravel/public/js/moderate.js
+++ laravel/public/js/moderate.js
@@ -304,18 +304,11 @@ $(document).ready(function($) {
     });
 
     $(document).on("submit", "#createbook_form", function(ev) {
-        ev.preventDefault ? ev.preventDefault() : (ev.returnValue = false);
         if(($("#book_file_create"))[0].files.length >   0) {
             if(($("#book_file_create"))[0].files[0].size >= 5000000) {
                 alert('Нельзя загрузить файл более 5мб');
                 return false;
             }
-            else {
-                return true;
-            }
-        }
-        else {
-            return true;
         }
     });
 }); 

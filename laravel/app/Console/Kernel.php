9ead5a72b8a5e4dd15cbe3f720ee262695993bff
shadow1512 <viacheslav.borisov@gmail.com>
2019-06-01 16:52:50 +0300

    индексацию поисковой таблицы вынесли

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        Commands\buildsearchindex::class
     ];
 
     /**
@@ -24,8 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('searchindex:create')->hourly();
     }
 
     /**

e80ec86200a1e7e4e03032ba6485a5d05ac90475
shadow1512 <viacheslav.borisov@gmail.com>
2019-06-10 01:24:41 +0300

    Ошибка в имени таблицы

--- laravel/app/Technical_Request.php
+++ laravel/app/Technical_Request.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Technical_Request extends Model
 {
     //
-    protected $table = 'technical_request';
+    protected $table = 'technical_requests';
 }

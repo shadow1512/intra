c7a1804e47d1c7a5ac368cf7abdf60962a92bc10
shadow1512 <viacheslav.borisov@gmail.com>
2019-06-10 03:16:14 +0300

    исправили название метода

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -65,7 +65,7 @@ class ProfileController extends Controller
         }
         $bills =   DB::table('users_dinner_bills')->selectRaw('MONTH(date_created) as mdc, MAX(summ) as ms')->where("user_id", "=",   Auth::user()->id)->groupBy('mdc')->limit(8)->get();
 
-        $tr     =   Technical_Request::where('user_id', '=',    Auth::user()->id)->order("created_at",  "desc")->limit(5)->get();
+        $tr     =   Technical_Request::where('user_id', '=',    Auth::user()->id)->orderByDesc("created_at")->limit(5)->get();
 
         return view('profile.view', ['contacts'    =>  $contacts,   'user'  =>  $user,  'dep'   =>  $dep,   'deps'  =>  $deps,  'ps'    =>  $ps,    'summ'  =>  $summ,  'bills' =>  $bills, 'requests'  =>  $tr]);
     }

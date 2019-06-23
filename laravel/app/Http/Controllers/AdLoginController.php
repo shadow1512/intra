8d531b0f1946d340de6ebbce192f3a088308dbc8
shadow1512 <viacheslav.borisov@gmail.com>
2019-06-23 15:44:33 +0300

    first не работает

--- laravel/app/Http/Controllers/AdLoginController.php
+++ laravel/app/Http/Controllers/AdLoginController.php
@@ -36,7 +36,7 @@ class AdLoginController extends Controller
             $login= mb_substr($login,    5);
         }
 
-        $user = Adldap::getProvider('default')->search()->where('cn',   '=',    $login)->first();
+        $user = Adldap::getProvider('default')->search()->where('cn',   '=',    $login)->get();
         var_dump($user);
         exit();
 

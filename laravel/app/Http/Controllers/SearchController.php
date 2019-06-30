cd0ed26b91e89be93807dee231e9910511b5feb5
shadow1512 <viacheslav.borisov@gmail.com>
2019-06-30 23:16:11 +0300

    не ищет по промежутку((

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -932,6 +932,7 @@ class SearchController extends Controller
                 ->leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
                 ->whereBetween(DB::raw("MONTH(birthday)"), [DB::raw("MONTH('$dt')"), DB::raw("MONTH('$dt1')")])
                 ->whereBetween(DB::raw("DAY(birthday)"), [DB::raw("DAY('$dt')"), DB::raw("DAY('$dt1')")])->get();
+            var_dump($birthday_records);
             $users_by_birthday  =   $birthday_records;
         }
         elseif (isset($bdates[0])    &&  trim($bdates[0])) {

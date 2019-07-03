9b987603c6e1288fbfccf26b9e5de32ed5b17c11
polya <polya@kodeks.ru>
2019-07-03 13:53:31 +0300

    Переработка списка "Дни рождения"

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -25,6 +25,7 @@
         <div class="h __h_m">Дни рождения</div>
         @if (count($users))
           <ul class="staff_ul __birthday">
+              <!-- <li class="staff_date"><strong>27 июня</strong></li> -->
               @foreach ($users as $user)
                   <li class="staff_li">
                     <a href="{{route('people.unit', ['id' => $user->id])}}" class="staff_lk"><img src="{{ $user->avatar }}" alt="" class="staff_img">

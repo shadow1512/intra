205c230f901cb00e23f33e7cfd6d325823d999ee
polya <polya@kodeks.ru>
2019-07-05 15:59:03 +0300

    #24966 Добавлена верстка модальных окон

--- laravel/resources/views/profile/view.blade.php
+++ laravel/resources/views/profile/view.blade.php
@@ -106,7 +106,7 @@
                     <div class="profile_form_info">
                       @if(!is_null($ps))
                         <div class="field_warning">
-                          Часть данных ожидает подтверждения модератором (<a href="">Орлова Е.И.</a>) перед внесением в корпоративный профиль. После подтверждения эти данные станут видны остальным сотрудникам.
+                          Часть данных ожидает подтверждения модератором (<a href="">Орлова Е.И.</a>) перед внесением в&nbsp;корпоративный профиль. После подтверждения эти данные станут видны остальным сотрудникам.
                         </div>
                       @endif
                         <div class="profile_form_info_left">
@@ -186,6 +186,74 @@
     </div>
 </div>
 <!--eo modal-->
+
+<!--modal-->
+<div class="overlay">
+  <div class="modal-w">
+    <div class="modal-cnt __changes">
+      <div class="modal_h"><a href="#" title="Закрыть" class="modal-close"></a></div>
+      <div class="profile_form_h">
+        <div class="h light_h __h_m">Отправлен запрос на&nbsp;внесение изменений<br />в&nbsp;ваш корпоративный профиль</div>
+      </div>
+      <div class="profile_form">
+        <ul class="lst-changes">
+          <li class="lst-changes_i">Фамилия: заменить &laquo;Иванова&raquo; на&nbsp;&laquo;Пушистик&raquo;</li>
+          <li class="lst-changes_i">E-mail: заменить &laquo;ivanova@kodeks.ru&raquo; на&nbsp;&laquo;xxx@mail.ru&raquo;</li>
+          <li class="lst-changes_i">Добавочный e-mail: добавить &laquo;xxx@mail.ru&raquo;</li>
+          <li class="lst-changes_i">Должность: добавить &laquo;Босс молокососс&raquo;</li>
+          <li class="lst-changes_i">Комната: добавить &laquo;столовая&raquo;</li>
+        </ul>
+      </div>
+      <div class="profile_form_submit">
+        <div class="h light_h __h_s __margin-bottom_l">Ожидайте подтверждения модератором: <a href="">Орлова Е.И.</a><br /> После подтверждения новые данные профиля станут видны остальным сотрудникам.</div>
+        <a href="#" class="btn profile_form_btn">Готово</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!--modal-->
+
+<!--modal-->
+<div class="overlay">
+  <div class="modal-w">
+    <div class="modal-cnt __changes">
+      <div class="modal_h"><a href="#" title="Закрыть" class="modal-close"></a></div>
+      <div class="profile_form_h">
+        <div class="h light_h __h_m">Отправлен запрос на&nbsp;внесение изменений<br />в&nbsp;ваш корпоративный профиль</div>
+      </div>
+      <div class="profile_form">
+        <ul class="lst-changes">
+          <li class="lst-changes_i">
+            Фамилия: заменить &laquo;Иванова&raquo; на&nbsp;&laquo;Пушистик&raquo;
+            <div class="lst-changes_error">Не&nbsp;соответствует информации в&nbsp;кадрах. Оставлено без изменений.</div>
+          </li>
+          <li class="lst-changes_i">
+            E-mail: заменить &laquo;ivanova@kodeks.ru&raquo; на&nbsp;&laquo;xxx@mail.ru&raquo;
+            <div class="lst-changes_error">Не&nbsp;соответствует корпоративному стандарту. Оставлено без изменений.</div>
+          </li>
+          <li class="lst-changes_i">
+            Добавочный e-mail: добавить &laquo;xxx@mail.ru&raquo;
+            <div class="lst-changes_approve">Внесено</div>
+          </li>
+          <li class="lst-changes_i">
+            Должность: добавить &laquo;Босс молокососс&raquo;
+            <div class="lst-changes_error">Не&nbsp;соответствует трудовому договору. Присвоено значение согласно приказу о&nbsp;назначении &laquo;Инженер-программист 2&nbsp;кат.&raquo;</div>
+          </li>
+          <li class="lst-changes_i">
+            Комната: добавить &laquo;столовая&raquo;
+            <div class="lst-changes_error">Не&nbsp;соответствует фактическому рабочему месту. Присвоено значение &laquo;к.666&raquo;</div>
+          </li>
+        </ul>
+      </div>
+      <div class="profile_form_submit">
+        <div class="h light_h __h_s __margin-bottom_l">Ожидайте подтверждения модератором: <a href="">Орлова Е.И.</a><br /> После подтверждения новые данные профиля станут видны остальным сотрудникам.</div>
+        <a href="#" class="btn profile_form_btn">Готово</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!--modal-->
+
 @endsection
 
 @section('dinner_bills')

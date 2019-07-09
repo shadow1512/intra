bfba269ca869e72feba7fe8a7472c0c558147323
shadow1512 <viacheslav.borisov@gmail.com>
2019-07-09 03:33:10 +0300

    Исправили логическую ошиюку

--- laravel/resources/views/profile/viewchanges.blade.php
+++ laravel/resources/views/profile/viewchanges.blade.php
@@ -12,22 +12,22 @@
                         @if($ps[$key])
                             @if(isset($user[$key]))
                                 @if($user[$key] &&  ($user[$key]   !=  $ps[$key]))
-                                    <li class="lst-changes_i">{{$value}}: заменить &laquo;@if($key  ==  "dep_id"){{$dep_old->name}} @else{{$user[$key]}} @endif;&raquo;&raquo; на&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_new->name}} @else{{$ps[$key]}} @endif;&raquo;</li>
+                                    <li class="lst-changes_i">{{$value}}: заменить &laquo;@if($key  ==  "dep_id"){{$dep_old->name}} @else{{$user[$key]}} @endif&raquo;&raquo; на&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_new->name}} @else{{$ps[$key]}} @endif&raquo;</li>
                                 @endif
                                 @if(!$user[$key])
-                                        <li class="lst-changes_i">{{$value}}: добавить&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_new->name}} @else{{$ps[$key]}} @endif;&raquo;</li>
+                                        <li class="lst-changes_i">{{$value}}: добавить&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_new->name}} @else{{$ps[$key]}} @endif&raquo;</li>
                                 @endif
                             @else
-                                <li class="lst-changes_i">{{$value}}: добавить&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_new->name}} @else{{$ps[$key]}} @endif;&raquo;</li>
+                                <li class="lst-changes_i">{{$value}}: добавить&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_new->name}} @else{{$ps[$key]}} @endif&raquo;</li>
                             @endif
                         @else
-                            <li class="lst-changes_i">{{$value}}: удалить&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_old->name}} @else{{$user[$key]}} @endif;&raquo;</li>
+                            <li class="lst-changes_i">{{$value}}: удалить&nbsp;&laquo;@if($key  ==  "dep_id"){{$dep_old->name}} @else{{$user[$key]}} @endif&raquo;</li>
                         @endif
                     @endforeach
                 </ul>
             </div>
             <div class="profile_form_submit">
-                <div class="h light_h __h_s __margin-bottom_l">Ожидайте подтверждения модератором@if(!is_null($moderator)): <a href="{{route('people.unit',   ["id"   =>  $moderator->id])}}">{{$moderate->lname}} {{mb_substr($moderate->fname, 0, 1, "UTF-8")}}. @if(!empty($moderate->mname)) {{mb_substr($moderate->mname, 0, 1, "UTF-8")}}.@endif</a><br /> После подтверждения новые данные профиля станут видны остальным сотрудникам.</div>
+                <div class="h light_h __h_s __margin-bottom_l">Ожидайте подтверждения модератором@if(!is_null($moderator)): <a href="{{route('people.unit',   ["id"   =>  $moderator->id])}}">{{$moderate->lname}} {{mb_substr($moderate->fname, 0, 1, "UTF-8")}}. @if(!empty($moderate->mname)) {{mb_substr($moderate->mname, 0, 1, "UTF-8")}}.@endif</a>@endif<br /> После подтверждения новые данные профиля станут видны остальным сотрудникам.</div>
                 <a href="#" class="btn profile_form_btn">Готово</a>
             </div>
         </div>

2ba1948f99821f0dfdf85fb95e94e6ea517cf26a
polya <polya@kodeks.ru>
2019-07-11 17:48:55 +0300

    #26617 Сделан вывод даты в стандартном формате в профиле пользователя

--- laravel/resources/views/users/unit.blade.php
+++ laravel/resources/views/users/unit.blade.php
@@ -16,16 +16,11 @@
                 <div class="profile_info_position">{{ $user->work_title }}</div>
             </div>
             <div class="profile_info_i">
-                <div class="profile_info_birth"><strong>Дата рождения:&nbsp;</strong><span>
-                        @php
-                            $months =   array("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря");
-                            $month  =   $months[date("n", strtotime($user->birthday))   -1];
-                            $day    =   date("j",   strtotime($user->birthday));
-                        @endphp {{ $day }} {{ $month }}</span></div>
-                <div class="profile_info_address"><strong>Адрес:&nbsp;</strong><span>{{ $user->address }}</span></div>
-                <div class="profile_info_room"><strong>Комната:&nbsp;</strong><span>{{ $user->room }}</span></div>
-                <div class="profile_info_phone"><strong>Телефон:&nbsp;</strong><span>{{ $user->phone }}</span></div>
-                <div class="profile_info_mail"><strong>E-mail: <a href='mailto:{{ $user->email }}'>{{ $user->email }}</a></strong></div>
+                @if(!empty($user->birthday))<div class="profile_info_birth"><strong>Дата рождения:&nbsp;</strong><span>{{ date("d.m.Y", strtotime($user->birthday)) }}</span></div>@endif
+                @if(!empty($user->address))<div class="profile_info_address"><strong>Адрес:&nbsp;</strong><span>{{ $user->address }}</span></div>@endif
+                @if(!empty($user->room))<div class="profile_info_room"><strong>Комната:&nbsp;</strong><span>{{ $user->room }}</span></div>@endif
+                @if(!empty($user->phone))<div class="profile_info_phone"><strong>Телефон:&nbsp;</strong><span>{{ $user->phone }}</span></div>@endif
+                @if(!empty($user->email))<div class="profile_info_mail"><strong>E-mail: <a href='mailto:{{ $user->email }}'>{{ $user->email }}</a></strong></div>@endif
             </div>
             <div class="profile_info_i">
                 @if($user->position_desc)<p class="profile_info_responsibility"><strong>Сфера компетенции:&nbsp;</strong><span>{{ $user->position_desc }}</span></p>@endif

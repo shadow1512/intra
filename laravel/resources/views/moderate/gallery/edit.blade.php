2b319bc1e19ef2e3a0373cc8fe7f042edc73a255
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-03 20:35:04 +0300

    расширили форму

--- laravel/resources/views/moderate/gallery/edit.blade.php
+++ laravel/resources/views/moderate/gallery/edit.blade.php
@@ -44,7 +44,7 @@
 
 
                             <div class="row fileupload-buttonbar">
-                                <div class="col-lg-7">
+                                <div class="col-lg-10">
                                     <!-- The fileinput-button span is used to style the file input field as button -->
                                     <span class="btn btn-success fileinput-button">
                                         <i class="glyphicon glyphicon-plus"></i>

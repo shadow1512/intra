fb8d079d30c35eb81a7d09bd53b3a236519a7d65
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-04 22:43:04 +0300

    Пытаемся поправить callback

--- laravel/public/js/moderate.js
+++ laravel/public/js/moderate.js
@@ -280,7 +280,7 @@ $(document).ready(function($) {
         success: function(e, data) {
         },
         done: function (result) {
-            $(this).call(this, $.Event('done'), { result: result });
+            $(this).fileupload().call(this, $.Event('done'), { result: result });
         },
         fail: function (e, data) {
 

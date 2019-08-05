ef28a1c15b0645d0720b42775b964f67825b3f98
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-05 04:29:11 +0300

    Вывели количество и обложку

--- laravel/app/Http/Controllers/GalleryController.php
+++ laravel/app/Http/Controllers/GalleryController.php
@@ -29,8 +29,9 @@ class GalleryController extends Controller
     public function index()
     {
         //галереи
-        $items = Gallery::select("gallery.*", "gallery_photos.image_th")
+        $items = Gallery::selectRaw("gallery.*, count(gallery_photos.id) as num, gallery_photos.image_th")
             ->leftJoin('gallery_photos',    'gallery.id',   '=',    'gallery_photos.gallery_id')
+            ->whereNull('gallery_photos.deleted_at')
             ->groupBy('gallery.id')
             ->orderBy('published_at', 'desc')->paginate(10);
         $items->withPath('/foto/');
--- laravel/resources/views/gallery/list.blade.php
+++ laravel/resources/views/gallery/list.blade.php
@@ -8,6 +8,7 @@
                 @foreach($items as $item)
                     <li class="news_li __important"><h3><a href="{{ route('foto.gallery', ['id' => $item->id])}}" class="news_li_lk">{{$item->name}}</a></h3>
                         <div class="news_li_date">{{date("d.m.Y", strtotime($item->published_at))}}</div>
+                        <div class="news_li_date">Количество фотографий: {{$item->num}}</div>
                         <div><a href="{{ route('foto.gallery', ['id' => $item->id])}}" class="news_li_lk"><img src="{{$item->image_th}}"/></a></div>
                     </li>
                 @endforeach

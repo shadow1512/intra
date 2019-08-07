6d1dfebaa913205710375bb077370b452f72f1ff
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-08 01:16:45 +0300

    Встаем на голову

--- laravel/app/Console/Commands/syncIssuesWithRedmine.php
+++ laravel/app/Console/Commands/syncIssuesWithRedmine.php
@@ -37,18 +37,8 @@ class syncIssuesWithRedmine extends Command
      *
      * @return mixed
      */
-    public function handle()
+    public function handle(Technical_Request $tr)
     {
-        require_once 'vendor/kbsali/redmine-api/lib/Redmine/Client.php';
-        $client =   new Redmine\Client(Config::get('redmine.host'), Config::get('redmine.login'), Config::get('redmine.password'));
-
-        $rec    =   $client->issue->all([
-                            'project_id'    =>  Config::get('redmine.project_id_oto'),
-                            'tracker_id'    =>  Config::get('redmine.tracker_id_oto'),
-                            'status_id'     =>  'closed',
-                            'cf_'   .   Config::get('redmine.cs_room')  =>  '205'
-                    ]);
-
-        var_dump($rec);
+        $tr->syncToRedmine();
     }
 }
--- laravel/app/Technical_Request.php
+++ laravel/app/Technical_Request.php
@@ -8,4 +8,19 @@ class Technical_Request extends Model
 {
     //
     protected $table = 'technical_requests';
+
+    public function syncToRedmine() {
+        //require_once 'vendor/kbsali/redmine-api/lib/Redmine/Client.php';
+        require_once 'vendor/autoload.php';
+        $client =   new Redmine\Client(Config::get('redmine.host'), Config::get('redmine.login'), Config::get('redmine.password'));
+
+        $rec    =   $client->issue->all([
+            'project_id'    =>  Config::get('redmine.project_id_oto'),
+            'tracker_id'    =>  Config::get('redmine.tracker_id_oto'),
+            'status_id'     =>  'closed',
+            'cf_'   .   Config::get('redmine.cs_room')  =>  '205'
+        ]);
+
+        var_dump($rec);
+    }
 }

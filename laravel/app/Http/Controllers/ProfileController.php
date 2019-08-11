055dbf290b492376b0e112cbeddf06f91b213cbc
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-11 23:58:19 +0300

    Выводим сотрудника УКОТ в интерфейс

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -73,7 +73,9 @@ class ProfileController extends Controller
         }
         $bills =   DB::table('users_dinner_bills')->selectRaw('MONTH(date_created) as mdc, MAX(summ) as ms')->where("user_id", "=",   Auth::user()->id)->groupBy('mdc')->limit(8)->get();
 
-        $tr     =   Technical_Request::where('user_id', '=',    Auth::user()->id)->orderByDesc("created_at")->limit(5)->get();
+        $tr     =   Technical_Request::select("technical_requests.*", "users.fname", "users.mname", "users.lname", "users.phone as sotr_phone")
+                                ->leftJoin('users',    'users.id', '=',    'technical_requests.assigned')
+                                ->where('user_id', '=',    Auth::user()->id)->orderByDesc("created_at")->limit(5)->get();
 
 
         return view('profile.view', ['contacts'    =>  $contacts,   'user'  =>  $user,  'dep'   =>  $dep,   'deps'  =>  $deps,  'ps'    =>  $ps,    'summ'  =>  $summ,  'bills' =>  $bills, 'requests'  =>  $tr,    'moderate'  =>  $moderate]);

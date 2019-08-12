4797d9c0923ed6dbd50bad82b5abbce8a4c9f64c
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-12 04:04:48 +0300

    Забыли поправить Kernel для команды

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
         Commands\makeXmlToCreateADStructure::class,
         Commands\syncIssuesWithRedmine::class,
         Commands\getIssueStatusesFromRedmine::class,
+        Commands\syncBookingsWithRedmine::class,
     ];
 
     /**

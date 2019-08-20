1069ad4e9d05cc09212a1354e6c8e29dd6f156f5
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-21 01:51:24 +0300

    динамическое подключение скриптов для галереи

--- laravel/resources/views/layouts/moderate.blade.php
+++ laravel/resources/views/layouts/moderate.blade.php
@@ -23,18 +23,22 @@
     <script src="{{ asset('js/libs/jquery-ui.min.js') }}"></script>
     <script src="{{ asset('js/libs/maskedinput.min.js') }}"></script>
     <script src="{{ asset('js/libs/jquery.datetimepicker.js') }}"></script>
-    <script src="{{ asset('js/libs/tmpl.min.js') }}"></script>
-    <script src="{{ asset('js/libs/load-image.all.min.js') }}"></script>
-    <script src="{{ asset('js/libs/canvas-to-blob.min.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.blueimp-gallery.min.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.iframe-transport.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload-process.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload-image.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload-audio.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload-video.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload-validate.js') }}"></script>
-    <script src="{{ asset('js/libs/jquery.fileupload-ui.js') }}"></script>
+    <script>
+        if(location.href.indexOf("foto")    !=  -1) {
+            document.write("<script src='{{ asset('js/libs/tmpl.min.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/load-image.all.min.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/canvas-to-blob.min.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.blueimp-gallery.min.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.iframe-transport.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload-process.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload-image.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload-audio.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload-video.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload-validate.js') }}'><\/srcipt>");
+            document.write("<script src='{{ asset('js/libs/jquery.fileupload-ui.js') }}'><\/srcipt>");
+        }
+    </script>
     <script src="{{ asset('js/moderate.js') }}"></script>
     <script src="{{ asset('js/libs/moderate-core.js') }}"></script>
 </head>

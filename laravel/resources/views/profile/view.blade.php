f35718607a26b53ab433f1c79eeb13a38ed5591e
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-26 01:25:55 +0300

    подготовка записей к показу правильному

--- laravel/resources/views/profile/view.blade.php
+++ laravel/resources/views/profile/view.blade.php
@@ -326,14 +326,40 @@
             <div class="modal_h"><a href="#" title="Закрыть" class="modal-close"></a></div>
             @foreach($changes as $item)
                 <div class="profile_form_h">
-                    <div class="h light_h __h_m">Отправлен запрос {{date("d.m.Y H:i:s", strtotime($item->created))}} на&nbsp;внесение изменений<br />в&nbsp;ваш корпоративный профиль</div>
+                    <div class="h light_h __h_m">Отправлен запрос {{date("d.m.Y H:i:s", strtotime($item->created_at))}} на&nbsp;внесение изменений<br />в&nbsp;ваш корпоративный профиль</div>
                 </div>
                 <div class="profile_form">
                     <ul class="lst-changes">
                         @if(isset($change_records[$item->id]))
                             @foreach($change_records[$item->id] as $record)
                                 <li class="lst-changes_i">
-                                    {{$labels[$record->field_name]}}: @if($record->old_value    &&  ($record->old_value !=  $record->new_value))заменить &laquo;{{$record->old_value}}&raquo; на&nbsp;&laquo;{{$record->new_value}}&raquo;@endif
+                                    @php
+                                        if($record->field_name  ==  "dep_id") {
+                                            if($record->old_value) {
+                                                $record->old_value  =   App\Dep::findOrFail($record->old_value)->value('name');
+                                            }
+                                            if($record->new_value) {
+                                                $record->new_value  =   App\Dep::findOrFail($record->new_value)->value('name');
+                                            }
+                                        }
+                                        if($record->field_name  ==  "chef") {
+                                            if($record->old_value   &&  !$record->new_value) {
+                                                $record->old_value  =   "Снять с руководящей должности";
+                                            }
+                                            if(!$record->old_value   &&  $record->new_value) {
+                                                $record->new_value  =   "Назначить руководителем";
+                                            }
+                                        }
+                                        if($record->field_name  ==  "workstart" ||  $record->field_name  ==  "birthday") {
+                                            if($record->old_value) {
+                                                $record->old_value  =   date("d.m.Y",   strtotime($record->old_value));
+                                            }
+                                            if($record->new_value) {
+                                                $record->new_value  =   date("d.m.Y",   strtotime($record->new_value));
+                                            }
+                                        }
+                                    @endphp
+                                    {{$labels[$record->field_name]}}: @if($record->old_value    &&  $record->new_value  &&  ($record->old_value !=  $record->new_value))заменить &laquo;{{$record->old_value}}&raquo; на&nbsp;&laquo;{{$record->new_value}}&raquo;@endif
                                     @if(!$record->old_value    &&  ($record->old_value !=  $record->new_value))добавить &laquo;{{$record->new_value}}&raquo;@endif
                                     @if($record->old_value    &&  !$record->new_value)удалить &laquo;{{$record->old_value}}&raquo;@endif
                                     @if($record->status==3)<div class="lst-changes_error">{{$record->reason}}</div>@endif

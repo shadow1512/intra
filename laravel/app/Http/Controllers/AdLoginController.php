aa924770d22959ca710fa60a942d83a3e4789bca
shadow1512 <viacheslav.borisov@gmail.com>
2019-08-29 00:49:32 +0300

    Убрали лишний файл

--- laravel/app/Http/Controllers/AdLoginController.php
+++ laravel/app/Http/Controllers/AdLoginController.php
@@ -41,7 +41,7 @@ class AdLoginController extends Controller
             $user = Adldap::getProvider('default')->search()->where('samaccountname',   '=',    $login)->first();
             if($user) {
                 $sid = $user->getConvertedSid();
-                $user = User::select("users.id")->leftJoin('user_keys', 'user_keys.user_id', '=', 'users.id')->where('user_keys.sid', '=', $sid)->limit(1)->first();
+                $user = User::where("sid",  "=",    $sid)->first();
                 if($user) {
                     Auth::loginUsingId($user->id, true);
                     return response()->json(['ok', $sid]);

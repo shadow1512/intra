5b59e7b26e975dcad06658235894d36aa25d128a
shadow1512 <viacheslav.borisov@gmail.com>
2019-09-03 01:46:01 +0300

    добавили переменных

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -93,11 +93,11 @@ class HomeController extends Controller
 
         //камеры
         $ch = curl_init('http://intra-unix.kodeks.net/img/cam1.jpg');
-        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FILETIME, true);
         $res = curl_exec($ch);
         $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $time   =   curl_getinfo($ch,   CURLINFO_FILETIME);
         $time   =   curl_getinfo($ch,   CURLINFO_FILETIME);
         if($status_code == 200) {
             var_dump($time);

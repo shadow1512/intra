4b9e4b47ebaf29feb3fd6ca522fcf1059958f0b9
shadow1512 <viacheslav.borisov@gmail.com>
2019-09-05 02:15:55 +0300

    правильное расположение сообщения

--- laravel/resources/views/errors/404.blade.php
+++ laravel/resources/views/errors/404.blade.php
@@ -1,3 +1,7 @@
 @extends('layouts.static', ['class'=>''])
 
-<h3>Страница, к которой вы обращаетесь не найдена</h3>
+@section('news')
+    <div class="content_i_w news">
+        <h1 class="h __h_m">Страница, к которой вы обращаетесь, не найдена</h1>
+    </div>
+@endsection

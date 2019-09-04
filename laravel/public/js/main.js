7191030ebca7757ecc846b37e3d57c0bd3f6ff3e
shadow1512 <viacheslav.borisov@gmail.com>
2019-09-04 03:00:05 +0300

    в динамику перенесли файлаплоад

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -331,6 +331,66 @@ popUp('.__js-modal-profile-lk', '.__js-modal-profile', function(but, win) {
                         $(this).val( first + move + '-' + lastfour );
                     }
                 });
+
+                $('#input_avatar').fileupload({
+                    dataType: 'json',
+                    url: $("#avatar_url").val(),
+                    singleFileUploads: false,
+                    sequentialUploads: true,
+                    //formData: [{name: 'opt', value: 'async'},{name: 'form',value: 'uploadfiles'}],
+                    submit: function (e, data) {
+                        totalSize = 0;
+
+                        $.each(data.files, function (index, file) {
+                            totalSize += file.size;
+                        });
+
+                        if(totalSize > 3000000) {
+                            alert("Для фотографии используйте изображение менее 3мб");
+                            $("#input_file_error").text("Нельзя загрузить более 3мб");
+                            $("#input_file_error").show();
+                            return false;
+                        }
+                        progress = document.createElement("div");
+                        $(progress).attr("id", "progress");
+                        $(progress).append("<div class=\"progressbar\" style=\"width: 0%;\" \>");
+                        $("div.profile_aside_pic").append(progress);
+                    },
+                    success: function(e, data) {
+                    },
+                    done: function (e, data) {
+                        $("#progress").remove();
+
+                        if(data.result[0] == "ok") {
+                            $("#img_avatar").attr("src", data.result[1]);
+                        }
+                        else {
+                            var errMessage = "Файл загружен не был. Причина - ";
+                            if(data.result[1] == "file wrong type") {
+                                errMessage += "для загрузки необходимо выбрать файл jpeg или png";
+                            }
+                            if(data.result[1] == "file too large") {
+                                errMessage += "для загрузки доступны изображения не более 3мб";
+                            }
+
+                            $("#input_file_error").text(errMessage);
+                            $("#input_file_error").show();
+                        }
+                    },
+                    fail: function (e, data) {
+                        $("#progress").remove();
+                        $("#input_file_error").text("Нам не удалось загрузить файлы. Мы уже знаем об этом и примем меры. Вы можете продолжить создание проекта, загрузив файлы позже через свой профиль, где вы сможете отредактировать создаваемый проект");
+                        $("#input_file_error").show();
+
+                    },
+                    progressall: function (e, data) {
+                        var progress = parseInt(data.loaded / data.total * 100, 10);
+                        $('#progress .progressbar').css(
+                            'width',
+                            progress + '%'
+                        );
+                    }
+                });
             }
         }
     });

7ccac8ca581f3ff6bb04b7360b467e1542d28a00
shadow1512 <viacheslav.borisov@gmail.com>
2019-10-15 00:33:10 +0300

    ищем, чего не хватает

--- laravel/resources/views/layouts/moderate.blade.php
+++ laravel/resources/views/layouts/moderate.blade.php
@@ -34,13 +34,13 @@
     @endif
     <script src='{{ asset('js/libs/jquery.iframe-transport.js') }}'></script>
     <script src="{{ asset('js/libs/jquery.fileupload.js') }}"></script>
+    <script src="{{ asset('js/libs/jquery.fileupload-process.js') }}"></script>
+    <script src="{{ asset('js/libs/jquery.fileupload-image.js') }}"></script>
     @if(Route::currentRouteName()   ==  'moderate.foto.edit')
-        <script src="{{ asset('js/libs/jquery.fileupload-process.js') }}"></script>
-        <script src="{{ asset('js/libs/jquery.fileupload-image.js') }}"></script>
         <script src='{{ asset('js/libs/jquery.fileupload-audio.js') }}'></script>
         <script src='{{ asset('js/libs/jquery.fileupload-video.js') }}'></script>
-        <script src='{{ asset('js/libs/jquery.fileupload-validate.js') }}'></script>
     @endif
+    <script src='{{ asset('js/libs/jquery.fileupload-validate.js') }}'></script>
     <script src='{{ asset('js/libs/jquery.fileupload-ui.js') }}'></script>
     <script src="{{ asset('js/moderate.js') }}"></script>
     <script src="{{ asset('js/libs/moderate-core.js') }}"></script>

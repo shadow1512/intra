743ff24ba234e8c2dd99944e76b51b500344d20f
shadow1512 <viacheslav.borisov@gmail.com>
2019-10-19 15:29:45 +0300

    Ошибка в названии переменной

--- laravel/resources/views/emails/bookingdeclined.blade.php
+++ laravel/resources/views/emails/bookingdeclined.blade.php
@@ -1,5 +1,5 @@
 <h2>Ваше бронирование &laquo;{{$booking->name}}, {{date("d.m.Y", strtotime($booking->date_book))}}, {{$booking->time_start}}&nbsp;&mdash;&nbsp;{{$booking->time_end}}&raquo; было отменено и удалено из расписания</h2>
 <p><strong>Причина:</strong>&nbsp;{{$booking->reason}}</p>
-@if($room->motify_email)
+@if($room->notify_email)
 <p><strong>Email ответственного:</strong>&nbsp;{{$room->notify_email}}</p>
 @endif

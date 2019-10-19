7c763e105b5e302c6a41fdddf1065b35f4f2d2ba
shadow1512 <viacheslav.borisov@gmail.com>
2019-10-19 14:39:38 +0300

    Layout поправили

--- laravel/resources/views/moderate/rooms/bookingslist.blade.php
+++ laravel/resources/views/moderate/rooms/bookingslist.blade.php
@@ -11,12 +11,12 @@
             @if (count($bookings))
             @foreach($bookings as $booking)
             <div class="row">
-                <div class="col-md-4">{{ $booking->name }}</div>
-                <div class="col-md-1">{{date("d.m.Y",   strtotime($booking->date_book))}}</div>
+                <div class="col-md-3">{{ $booking->name }}</div>
+                <div class="col-md-2">{{date("d.m.Y",   strtotime($booking->date_book))}}</div>
                 <div class="col-md-1">{{ $booking->time_start}}</div>
                 <div class="col-md-1">{{ $booking->time_end}}</div>
-                <div class="col-md-3">{{ $booking->lname }} {{mb_substr($booking->fname,    0,  1,  "UTF-8")}}.@if ($booking->mname) {{mb_substr($booking->mname,    0,  1,  "UTF-8")}}. @endif <br/>@if ($booking->phone) {{$booking->phone}},<br/>@endif @if ($booking->email) {{$booking->email}} @endif</div>
-                <div class="col-md-1"><a href="{{ route('moderate.rooms.bookingconfirm', ["id" => $booking->id, "approved"    =>  1]) }}">Подтвердить</a></div>
+                <div class="col-md-2">{{ $booking->lname }} {{mb_substr($booking->fname,    0,  1,  "UTF-8")}}.@if ($booking->mname) {{mb_substr($booking->mname,    0,  1,  "UTF-8")}}. @endif <br/>@if ($booking->phone) {{$booking->phone}},<br/>@endif @if ($booking->email) {{$booking->email}} @endif</div>
+                <div class="col-md-2"><a href="{{ route('moderate.rooms.bookingconfirm', ["id" => $booking->id, "approved"    =>  1]) }}">Подтвердить</a></div>
                 <div class="col-md-1"><a href="{{ route('moderate.rooms.bookingconfirm', ["id" => $booking->id, "approved"    =>  2]) }}">Отклонить</a></div>
             </div>
             @endforeach

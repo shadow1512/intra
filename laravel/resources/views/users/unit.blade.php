40efcde033d9379ddf7a2fb6b8e13bf57bde210e
shadow1512 <viacheslav.borisov@gmail.com>
2019-11-14 00:58:45 +0300

    Дата рождения в карточке - убираем год, числовой месяц меняем на строковый

--- laravel/resources/views/users/unit.blade.php
+++ laravel/resources/views/users/unit.blade.php
@@ -16,7 +16,13 @@
                 <div class="profile_info_position">{{ $user->work_title }}</div>
             </div>
             <div class="profile_info_i">
-                @if(!empty($user->birthday))<div class="profile_info_birth"><strong>Дата рождения:&nbsp;</strong><span>{{ date("d.m.Y", strtotime($user->birthday)) }}</span></div>@endif
+                @if(!empty($user->birthday))<div class="profile_info_birth"><strong>Дата рождения:&nbsp;</strong><span>
+                        @php
+                            $months =   array("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря");
+                            $month  =   $months[date("n", strtotime($user->birthday))   -1];
+                            $day    =   date("j",   strtotime($user->birthday));
+                        @endphp
+                        {{ $day }}{{ $month }}</span></div>@endif
                 @if(!empty($user->address))<div class="profile_info_address"><strong>Адрес:&nbsp;</strong><span>{{ $user->address }}</span></div>@endif
                 @if(!empty($user->room))<div class="profile_info_room"><strong>Комната:&nbsp;</strong><span>{{ $user->room }}</span></div>@endif
                 @if(!empty($user->phone))<div class="profile_info_phone"><strong>Телефон:&nbsp;</strong><span>{{ $user->phone }}</span></div>@endif

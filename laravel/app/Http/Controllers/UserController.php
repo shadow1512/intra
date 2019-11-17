e932768481a5d93ce63791de079a259fe6d18a2c
shadow1512 <viacheslav.borisov@gmail.com>
2019-11-18 01:21:17 +0300

    отображаем по старому: выводим список сотрудников, которые привязаны непосредственно к подразделению

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -90,10 +90,18 @@ class UserController extends Controller
 
         if(!is_null($id)) {
             $currentDep     = Dep::findOrFail($id);
-            $users = User::leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
+            /*$users = User::leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
                 ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef', 'deps.parent_id')
                 ->leftJoin('deps', 'deps_peoples.dep_id', '=', 'deps.id')
                 ->whereRaw("deps_peoples.dep_id IN (SELECT id FROM deps WHERE parent_id LIKE '" . $currentDep->parent_id . "%')")
+                ->orderByRaw('IFNULL(deps_peoples.chef,1000)', 'asc')->orderByRaw('LENGTH(parent_id)',  'asc')->orderBy('users.lname', 'asc')
+                ->limit(200)->get();*/
+
+            //Пока вернемся к старой структуре, когда пользователей видим только тех, что привязаны непосредственно к подразделению
+            $users = User::leftJoin('deps_peoples', 'users.id', '=', 'deps_peoples.people_id')
+                ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef', 'deps.parent_id')
+                ->leftJoin('deps', 'deps_peoples.dep_id', '=', 'deps.id')
+                ->whereRaw("deps_peoples.dep_id = $id")
                 ->orderByRaw('IFNULL(deps_peoples.chef,1000)', 'asc')->orderByRaw('LENGTH(parent_id)',  'asc')->orderBy('users.lname', 'asc')
                 ->limit(200)->get();
         }
@@ -103,7 +111,7 @@ class UserController extends Controller
                 ->select('users.*', 'deps.name as depname', 'deps.id as depid', 'deps_peoples.work_title', 'deps_peoples.chef')
                 ->leftJoin('deps', 'deps_peoples.dep_id', '=', 'deps.id')
                 ->orderByRaw('IFNULL(deps_peoples.chef,1000)', 'asc')->orderByRaw('LENGTH(parent_id)',  'asc')->orderBy('users.lname', 'asc')
-                ->limit(100)->get();
+                ->limit(120)->get();
         }
 
         if(!is_null($id)) {

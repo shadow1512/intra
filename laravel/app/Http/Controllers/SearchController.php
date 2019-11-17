879f50369748b2d75f3e3730264da67170dfaf8e
shadow1512 <viacheslav.borisov@gmail.com>
2019-11-17 18:23:28 +0300

    не находит точку??

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -1080,6 +1080,7 @@ class SearchController extends Controller
         elseif (isset($bdates[0])    &&  trim($bdates[0])) {
             //Ситуация, когда вводят руками
             if(!mb_strrpos(".",  trim($bdates[0]))) {
+                echo "unformatted\r\n";
                 $startDate  =   $this->getDatePartsFromString($bdates[0]);
                 list($startDay, $startMonth)    =   $startDate;
                 if(is_null($startDay)) {

eb17afc1f71a3d58398bc38ddf38dfe2f3432271
shadow1512 <viacheslav.borisov@gmail.com>
2020-03-25 02:33:55 +0300

    рулетка, input или file

--- laravel/app/Http/Controllers/ModerateController.php
+++ laravel/app/Http/Controllers/ModerateController.php
@@ -1223,7 +1223,7 @@ class ModerateController extends Controller
     public function userscropavatar($id, Request $request)
     {
         var_dump($request->all());exit();
-        $path   =   Storage::disk('public')->putFile(Config::get('image.avatar_path'), $request->file('data'), 'public');
+        $path   =   Storage::disk('public')->putFile(Config::get('image.avatar_path'), $request->input('data'), 'public');
         $type   =   Storage::disk('public')->getMimetype($path);
 
         if($type == "image/jpeg" || $type == "image/pjpeg" || $type == "image/png") {

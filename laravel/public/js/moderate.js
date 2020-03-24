f66a2bb60de4862640e17d6bc93d9fe1517f09d2
shadow1512 <viacheslav.borisov@gmail.com>
2020-03-25 02:26:06 +0300

    вернули contentType: false

--- laravel/public/js/moderate.js
+++ laravel/public/js/moderate.js
@@ -202,13 +202,14 @@ $(document).ready(function($) {
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-                    'Content-Type': 'multipart/form-data'
+                    //'Content-Type': 'multipart/form-data'
                 },
                 method: "PUT",
                 url: url,
                 data: fd,
                 async: false,
                 processData: false,
+                contentType: false,
                 success: function (msg) {
                     alert(msg);
                 }

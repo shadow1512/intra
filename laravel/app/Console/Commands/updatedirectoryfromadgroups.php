1ec50537c59f91b80239896826186130fd171fe1
shadow1512 <viacheslav.borisov@gmail.com>
2020-04-17 00:19:14 +0300

    неправильно название указано

--- laravel/app/Console/Commands/updatedirectoryfromadgroups.php
+++ laravel/app/Console/Commands/updatedirectoryfromadgroups.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 use Config;
 use DB;
 
-class updatedirectoryfromad extends Command
+class updatedirectoryfromadgroups extends Command
 {
     /**
      * The name and signature of the console command.

97d62fa6c4757d04fdbd20a315ed3870881fb43d
shadow1512 <viacheslav.borisov@gmail.com>
2020-04-21 23:06:28 +0300

    пытаемся понять, почему файла нет, где надо

--- laravel/app/Http/Controllers/ModerateController.php
+++ laravel/app/Http/Controllers/ModerateController.php
@@ -882,6 +882,7 @@ class ModerateController extends Controller
             }
 
             $files = $request->file('photo_files');
+            var_dump($files);
             $filename = microtime() . "." . $files[0]->extension();
             $path_full = Storage::disk('public')->putFileAs(Config::get('image.gallery_path') . '/' . $id, $files[0], $filename, 'public');
             $type = Storage::disk('public')->getMimetype($path_full);

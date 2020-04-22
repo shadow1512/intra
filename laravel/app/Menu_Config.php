c61e67dd1288f52c6cdd5b1df87c63964027a933
shadow1512 <viacheslav.borisov@gmail.com>
2020-04-22 18:28:31 +0300

    неправильное наименование колонки

--- laravel/app/Menu_Config.php
+++ laravel/app/Menu_Config.php
@@ -11,11 +11,11 @@ class Menu_Config extends Model
 
     public static function getLevel($id, $items) {
         if(is_null($id)) {
-            $level_items    =   Menu_Config::where('hidden',   '=',    0)->whereNull('parent_id')->orderBy('sort',   'desc')->get();
+            $level_items    =   Menu_Config::where('visible',   '=',    1)->whereNull('parent_id')->orderBy('sort',   'desc')->get();
             $items['root']  =   $level_items;
         }
         else {
-            $level_items    =   Menu_Config::where('hidden',   '=',    0)->where('parent_id',  '=',    $id)->orderBy('sort',   'desc')->get();
+            $level_items    =   Menu_Config::where('visible',   '=',    1)->where('parent_id',  '=',    $id)->orderBy('sort',   'desc')->get();
             $items[$id]  =   $level_items;
         }
 

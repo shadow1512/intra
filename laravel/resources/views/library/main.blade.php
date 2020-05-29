9529706a619f3767cd01b968bec63a3cd8f85003
polya <polya@kodeks.ru>
2020-05-29 12:47:39 +0300

    Правки в разделе библиотека

--- laravel/resources/views/library/main.blade.php
+++ laravel/resources/views/library/main.blade.php
@@ -30,12 +30,7 @@
         @endif
     </div>
     <div class="content_i inside-page">
-        <div class="content_i_w @if (!is_null($curRazdel)) __border_top @endif">
-            @if (is_null($curRazdel))
-            <div class="content_i_header">
-                <div class="h __h_m">Вся библиотека</div>
-            </div>
-            @endif
+        <div class="content_i_w __border_top">
             <div class="content_tx __no-pad">
                 @if (count($books))
                 <ul class="library_lst">

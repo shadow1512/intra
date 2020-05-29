6dec5d0f22dd9cd10b5ce16a562f0b1ec3f4c0eb
shadow1512 <viacheslav.borisov@gmail.com>
2020-05-30 02:21:10 +0300

    изменил название переменной

--- laravel/resources/views/users/search.blade.php
+++ laravel/resources/views/users/search.blade.php
@@ -150,8 +150,8 @@
                             <!--<div class="directory_lst_i_name_status"></div>-->
                             </div>
                             <div class="directory_lst_i_info">
-                                <div class="directory_lst_i_info_i">Местный телефон: {{$contact->phone}}</div>
-                                @if($contact->mobile_phone)<div class="directory_lst_i_info_i">Мобильный телефон: {{$contact->mobile_phone}}</div>@endif
+                                <div class="directory_lst_i_info_i">Местный телефон: {{$user->phone}}</div>
+                                @if($contact->mobile_phone)<div class="directory_lst_i_info_i">Мобильный телефон: {{$user->mobile_phone}}</div>@endif
                                 <div class="directory_lst_i_info_i">Комната: {{$user->room}}</div>
                                 <div class="directory_lst_i_info_i"><a href="mailto:{{$user->email}}">{{$user->email}}</a></div>
                             </div>

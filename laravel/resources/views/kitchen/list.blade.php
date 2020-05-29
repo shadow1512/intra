ccae10a0236df448b6dd84d2fb4dae122145ed90
shadow1512 <viacheslav.borisov@gmail.com>
2020-05-29 17:38:21 +0300

    убрали дефолтный период и все выводим

--- laravel/resources/views/kitchen/list.blade.php
+++ laravel/resources/views/kitchen/list.blade.php
@@ -27,15 +27,15 @@
             <div class="reserve_h_date">{{$caldate->format("j")}} {{$month_names[$caldate->format("n") - 1]}}, {{$day_names[$caldate->format("N")-1]}}</div>
         </div>
         <div class="reserve-lst">
-            <div class="reserve-lst_i __current">
+            <!--<div class="reserve-lst_i __current">
                 <div class="reserve-lst_time">{{$periods[$active_period]}} - {{\Carbon\Carbon::parse($periods[$active_period])->addMinutes(15)->format("H:i")}}</div>
                 <ul class="reserve-lst_name">
                     @foreach($bookings_by_time[$periods[$active_period]] as $booking)
                         <li class="reserve-lst_name_i"><strong>{{$booking->lname}}</strong> {{$booking->fname}} {{$booking->mname}}</li>
                     @endforeach
                 </ul>
-            </div>
-            @for($i=0;  $i  <   $active_period; $i++)
+            </div>-->
+            @for($i=0;  $i  <   $total_periods  -   1; $i++)
             <div class="reserve-lst_i">
                 <div class="reserve-lst_time">{{$periods[$i]}} - {{\Carbon\Carbon::parse($periods[$i])->addMinutes(15)->format("H:i")}}</div>
                 <ul class="reserve-lst_name">

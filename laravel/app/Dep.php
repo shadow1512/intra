5fbbbe195afcdae68c6b9f6058fad8238bf45c10
shadow1512 <viacheslav.borisov@gmail.com>
2020-08-19 01:36:23 +0300

    не работает определение модератора

--- laravel/app/Dep.php
+++ laravel/app/Dep.php
@@ -29,6 +29,7 @@ class Dep extends Model
      */
 
     public static function getModerate($id) {
+        DB::enableQueryLog(); // Enable query log
         $dep    =   parent::findOrFail($id);
         if(mb_strlen($dep->parent_id,   "UTF-8")    >=   2) {
             $code   =   $dep->parent_id;
@@ -40,7 +41,7 @@ class Dep extends Model
                 $code   =   mb_substr($code,  0,  mb_strlen($code,   "UTF-8")   -   2);
             }
         }
-
+        dd(DB::getQueryLog());
         return null;
     }
 

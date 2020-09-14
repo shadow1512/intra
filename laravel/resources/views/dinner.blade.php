5b35991683fa373806b7e805f866f503ec0a21ca
shadow1512 <viacheslav.borisov@gmail.com>
2020-09-15 02:33:48 +0300

    третий плеер

--- laravel/resources/views/dinner.blade.php
+++ laravel/resources/views/dinner.blade.php
@@ -86,8 +86,8 @@
                 <div class="dinner">
                     <div class="h __h_m">Столовая</div>
                     <div class="dinner_camera">
-                        <div class="dinner_camera_i"><h3>Камера 2</h3><div id="rtmp_cam1"></div></div>
-                        <div class="dinner_camera_i"><h3>Камера 1</h3><div id="rtmp_cam2"></div></div>
+                        <div class="dinner_camera_i"><h3>Камера 1</h3><div id="rtmp_cam1"></div></div>
+                        <div class="dinner_camera_i"><h3>Камера 2</h3><div id="rtmp_cam2"></div></div>
                         <div class="dinner_camera_i"><h3>Коридор</h3><div id="rtmp_cam3"></div></div>
                     </div>
                 </div>

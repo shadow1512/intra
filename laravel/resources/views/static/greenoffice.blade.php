320f2a5ee7a437afe3685dd102b027b9cdd8e887
shadow1512 <viacheslav.borisov@gmail.com>
2020-12-22 03:10:14 +0300

    Мелкие правки по внешнему виду

--- laravel/resources/views/static/greenoffice.blade.php
+++ laravel/resources/views/static/greenoffice.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.appmenu')
 
 @section('content')
-    <div class="main_news">
+<div class="content_i_w news">
         <div class="h __h_m">{{$page->title}}</div>
-        {{!! $page->body !!}}
-    </div>
+        <div class="news_i">{!! $page->body !!}</div>
+</div>
 @endsection

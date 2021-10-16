7b38f97db907ad4e4668a643b02b001fc2e01505
shadow1512 <viacheslav.borisov@gmail.com>
2021-10-17 01:49:22 +0300

    withTrashed

--- laravel/app/Console/Commands/commiteridfiller.php
+++ laravel/app/Console/Commands/commiteridfiller.php
@@ -96,8 +96,7 @@ class commiteridfiller extends Command
 
             if(count($moderate)) {
                 foreach ($moderate as $moderator) {
-                    echo $item->id . "-"    .   $moderator->id  .   "\r\n";
-                    Profiles_Saved_Data::where("id",   "=",    $item->id)->update(["commiter_id"  =>  $moderator->id]);
+                    Profiles_Saved_Data::withTrashed()->where("id",   "=",    $item->id)->update(["commiter_id"  =>  $moderator->id]);
                     break;
                 }
             }

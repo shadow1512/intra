3efc30dc9078f4fe956e1cd7a821fa7c24e4099e
shadow1512 <viacheslav.borisov@gmail.com>
2021-10-17 01:27:25 +0300

    херня со статикой

--- laravel/app/Console/Commands/commiteridfiller.php
+++ laravel/app/Console/Commands/commiteridfiller.php
@@ -96,7 +96,7 @@ class commiteridfiller extends Command
 
             if(count($moderate)) {
                 foreach ($moderate as $moderator) {
-                    Profiles_Saved_Data::update(["commiter_id"  =>  $moderator->id])->where("id",   "=",    $item->id);
+                    Profiles_Saved_Data::where("id",   "=",    $item->id)->update(["commiter_id"  =>  $moderator->id]);
                     break;
                 }
             }

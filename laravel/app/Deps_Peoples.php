87f1f0efca74e2f5cfc02e12254fe2ccd15c0085
shadow1512 <viacheslav.borisov@gmail.com>
2022-03-12 14:02:45 +0300

    добавили softdeletes на связь между человеком и подразделением

--- laravel/app/Deps_Peoples.php
+++ laravel/app/Deps_Peoples.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Deps_Peoples extends Model
 {
     //
+    use SoftDeletes;
+
     protected $table = 'deps_peoples';
 }

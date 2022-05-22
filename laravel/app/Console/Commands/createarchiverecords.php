d0cd374b46176d3340e4fbab2ec413bdf0585fc7
shadow1512 <viacheslav.borisov@gmail.com>
2022-05-22 17:49:12 +0300

    поправочка

--- laravel/app/Console/Commands/createarchiverecords.php
+++ laravel/app/Console/Commands/createarchiverecords.php
@@ -167,7 +167,7 @@ class createarchiverecords extends Command
     public function handle()
     {
         //
-        $unique_users   =   array();
+    $unique_users   =   array("lname"   =>array(),  "mname" =>  array(),    "fname" =>  array());
         $importData =   $this->loader(array(Config::get('archiveexcel.2016'), Config::get('archiveexcel.2017')));
         foreach($importData as $org =>  $deps_data) {
             foreach ($deps_data as $dep =>  $user_data) {

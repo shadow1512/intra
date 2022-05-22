e5b59c778013b6da6a16c2315a1e9b33841f9143
shadow1512 <viacheslav.borisov@gmail.com>
2022-05-22 17:23:58 +0300

    сортировка для более удобного сравнения

--- laravel/app/Console/Commands/createarchiverecords.php
+++ laravel/app/Console/Commands/createarchiverecords.php
@@ -153,8 +153,10 @@ class createarchiverecords extends Command
     {
         //
         $importData =   $this->loader(array(Config::get('archiveexcel.2016'), Config::get('archiveexcel.2017')));
+        ksort($importData);
         foreach($importData as $org =>  $deps_data) {
             echo "$org\r\n";
+            ksort($deps_data);
             foreach ($deps_data as $dep =>  $user_data) {
                 echo "----$dep\r\n";
             }

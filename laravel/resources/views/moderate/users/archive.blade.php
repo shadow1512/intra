1616c265ef4be0734c4e2a94d4fc28f56c9f0e68
shadow1512 <viacheslav.borisov@gmail.com>
2022-05-22 20:47:23 +0300

    разметка

--- laravel/resources/views/moderate/users/archive.blade.php
+++ laravel/resources/views/moderate/users/archive.blade.php
@@ -7,8 +7,10 @@
 
                 <div class="row">
                     <div class="col-md-9"><h3>Архив (Перейти в <a href="{{ route('moderate.users.start')}}">список действующих сотрудников</a>)</h3></div>
-                    <form name="searcharive" class="form-horizontal" method="POST" action="{{ route('moderate.users.archive.search' }}"><div class="col-md-2"><input type="text" name="searcharchive" value="" id="searcharchive"/></div>
-                        <div class="col-md-1"><input type="submit" name="searcharchive_button" value="Искать" id="searcharchive_button"/></div></form>
+                    <form name="searcharive" class="form-horizontal" method="POST" action="{{ route('moderate.users.archive.search' }}">
+                        <div class="col-md-2"><input type="text" name="searcharchive" value="" id="searcharchive"/></div>
+                        <div class="col-md-1"><input type="submit" name="searcharchive_button" value="Искать" id="searcharchive_button"/></div>
+                    </form>
                 </div>
                 @if ($mode  ==  'letters')
                     <div class="row">
@@ -42,28 +44,28 @@
                             <a href="{{ route('moderate.users.archive', ['letter' =>  'Э'])}}">Э</a>
                             <a href="{{ route('moderate.users.archive', ['letter' =>  'Ю'])}}">Ю</a>
                             <a href="{{ route('moderate.users.archive', ['letter' =>  'Я'])}}">Я</a>
-                            <div>
-                            </div>
-                            @endif
-                            @if (count($users))
-                            <div class="content_tx __no-pad">
-                                <ul class="directory_lst">
-                                    @foreach($users as $item)
-                                        <li class="directory_lst_i @if (mb_substr($item->birthday,  5) ==  date("m-d")) __birthday @endif">
-                                            <div class="directory_lst_i_pic"><img src="@if($item->avatar_round){{$item->avatar_round}} @else {{$item->avatar}} @endif" class="directory_lst_i_img" title="{{ date("d.m.Y", strtotime($item->birthday)) }}"></div>
-                                            <div class="directory_lst_i_name">{{ $item->lname }} {{ $item->fname }} {{ $item->mname }}
-                                                <div class="directory_lst_i_name_spec">{{$item->worktitle}}</div>
-                                            </div>
-                                            <div class="directory_lst_i_info">
-                                                @if($item->mobile_phone)<div class="directory_lst_i_info_i">Мобильный тел.: {{$item->mobile_phone}}</div>@endif
-                                                    <div class="directory_lst_i_info_i">Дата увольнения: {{ date("d.m.Y", strtotime($item->deleted_at)) }}</div>
-                                            </div>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            @endif
                         </div>
                     </div>
+                @endif
+                @if (count($users))
+                <div class="content_tx __no-pad">
+                    <ul class="directory_lst">
+                        @foreach($users as $item)
+                            <li class="directory_lst_i @if (mb_substr($item->birthday,  5) ==  date("m-d")) __birthday @endif">
+                                <div class="directory_lst_i_pic"><img src="@if($item->avatar_round){{$item->avatar_round}} @else {{$item->avatar}} @endif" class="directory_lst_i_img" title="{{ date("d.m.Y", strtotime($item->birthday)) }}"></div>
+                                <div class="directory_lst_i_name">{{ $item->lname }} {{ $item->fname }} {{ $item->mname }}
+                                    <div class="directory_lst_i_name_spec">{{$item->worktitle}}</div>
+                                </div>
+                                <div class="directory_lst_i_info">
+                                    @if($item->mobile_phone)<div class="directory_lst_i_info_i">Мобильный тел.: {{$item->mobile_phone}}</div>@endif
+                                        <div class="directory_lst_i_info_i">Дата увольнения: {{ date("d.m.Y", strtotime($item->deleted_at)) }}</div>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
+        </div>
+    </div>
 @endsection

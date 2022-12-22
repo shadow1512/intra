c96b20a62a5b35b3fdf8162575e349fecb05d3d3
shadow1512 <viacheslav.borisov@gmail.com>
2022-12-22 15:19:50 +0300

    в контактах ссылка

--- laravel/resources/views/profile/contacts.blade.php
+++ laravel/resources/views/profile/contacts.blade.php
@@ -9,7 +9,7 @@
             <!--<div class="profile_contacts_status"></div>--><a href="{{ route('people.unit', ['id' => $item->id])}}" class="profile_contacts_name">{{$item->lname}} {{$item->fname}} {{$item->mname}}</a>
                 <div class="profile_contacts_position">{{$item->work_title}}</div>
                 <div class="profile_contacts_position">E-mail: <a href="mailto:{{$item->email}}">{{$item->email}}</a></div>
-                <div class="profile_contacts_position">Местный тел.: @if($item->ip_phone) {{$item->ip_phone}} @if($item->phone) или {{$item->phone}} @endif @else {{$item->phone}} @endif</div>
+                @if($item->ip_phone || $item->phone)<div class="profile_contacts_position">Местный тел.: @if($item->ip_phone) @if(Auth::check() && !is_null(Auth::user()->ip_phone)) <a href="{{route("people.call", ["id"   =>  $item->id])}}" class="dynamic_call">{{$item->ip_phone}}</a> @else {{$item->ip_phone}} @endif @if($item->phone) или {{$item->phone}} @endif @else {{$item->phone}} @endif</div>@endif
                 @if($item->mobile_phone)<div class="profile_contacts_position">Мобильный тел.: {{$item->mobile_phone}}</div>@endif
             </div>
         </li>

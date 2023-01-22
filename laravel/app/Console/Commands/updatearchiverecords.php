5b6941df690f2c6fe150f1b9555a0f26a8863543
shadow1512 <viacheslav.borisov@gmail.com>
2023-01-22 21:32:39 +0300

    последняя логика

--- laravel/app/Console/Commands/updatearchiverecords.php
+++ laravel/app/Console/Commands/updatearchiverecords.php
@@ -55,19 +55,43 @@ class updatearchiverecords extends Command
             $work   =   trim($filedata[6]);
             $work_start =   trim($filedata[7]);
             $work_end   =   trim($filedata[8]);
-            
+            $updated    =   0;
+            $skipped_not_deleted        =   0;
+            $skipped_already_has        =   0;   
+            $flag_create    =   0;
             //ищем среди всех сотрудников тех, что есть в списке как удаленные
             $obj    =   User::onlyTrashed()->where("fname",    "=",    $fname)->where("lname", "=",    $lname)->where("mname", "=",    $mname)->get();
             if($obj->count()    ==  1) {
                 if(is_numeric($dep)) {
                     $work_place= Deps_Peoples::where("people_id", "=",    $obj->first()->id)->orderBy("created_at", "desc")->get();
                     if($work_place->count()   >   0) {
-                        echo $lname .   " " .   $fname  .   " " .   $mname  .   " || "   .   $obj->first()->id . " - есть место работы\r\n";
-                        echo $work_place->first()->work_title . " ||| " . $work . "\r\n\r\n";
-                        echo $work_place->first()->dep_id . " ||| " . $dep . "\r\n\r\n";
+                        $current_work   =   $work_place->last();
+                        if(($current_work->work_title    !== $work) &&  ($current_work->dep_id   !=  $dep)) {
+                            $current_work->delete();
+                            $flag_create    =   1;
+                        }
+                        else {
+                            $skipped_already_has++;
+                        }
+                        
                     }
                     else {
-                        echo $lname .   " " .   $fname  .   " " .   $mname  .   " || GOOD \r\n";
+                        $flag_create    =   1;
+                    }
+                    
+                    if($flag_create) {
+                        $archive_work   =   new Deps_Peoples();
+                        $archive_work->people_id    =   $obj->first()->id;
+                        $archive_work->dep_id       =   $dep;
+                        $archive_work->work_title   =   $work;
+                        $archive_work->save();
+
+                        $obj->first()->work_start   =   $work_start;
+                        $obj->first()->work_end     =   $work_end;
+                        $obj->fitst()->deleted_at   =   $work_end   .   " 00:00:00";
+                        $obj->first()->save();
+
+                        $updated ++;
                     }
                 }
                 else {
@@ -76,8 +100,10 @@ class updatearchiverecords extends Command
                 }
             }
             else {
-                echo $lname .   " " .   $fname  .   " " .   $mname  .   " || не обработано:"   .   $obj->count() . "\r\n";
+                $skipped_not_deleted++;
             }
         }
+        
+        echo "Updated: $updated\r\n Skipped because already has: $skipped_already_has\r\n Skipped because they are alive: $skipped_not_deleted\r\n";
     }
 }

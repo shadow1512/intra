c340a62c694ec3fa66c24e7e75b677d1216f128d
shadow1512 <viacheslav.borisov@gmail.com>
2023-02-20 00:25:26 +0300

    дебаг взвешивания

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -986,6 +986,7 @@ class SearchController extends Controller
                 }
 
                 $user_ids = array_keys($search_result['users']);
+                var_dump($user_ids);
                 //Убираем лишние результаты поиска по более, чем одному слову
                 $max_weight =   0;
                 foreach($user_ids as $user_id) {

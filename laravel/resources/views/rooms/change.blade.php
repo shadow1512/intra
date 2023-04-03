46c2dfe01948292cce7fbe70aec773147101e3b3
astreniya <astreniya@kodeks.ru>
2023-04-03 14:37:18 +0300

    FIX форма бронирования переговорных

--- laravel/resources/views/rooms/change.blade.php
+++ laravel/resources/views/rooms/change.blade.php
@@ -85,7 +85,7 @@
       <!--<label class="lbl">Используемое ПО:</label>-->
       <div class="">
         <div class="form-check form-check-inline">
-          <input type="checkbox" class="form-check-input ich" id="check9_software_change" name="software_ukotman_change" value="1" @if($booking->software_ukotman) checked="checked" @endif>
+          <input type="checkbox" class="form-check-input ich" id="check9_software_change" name="software_ukotman_change" value="1">
 		  <label class="lbl form-check-label" for="check9_software_change">Требуется присутствие специалиста УКОТ на&nbsp;мероприятии</label>
           <!--<input type="checkbox" class="form-check-input ich" id="check5_software_change" name="software_skype_change" value="1" @if($booking->software_skype) checked="checked" @endif>
           <label class="lbl form-check-label" for="check5_software_change">Skype</label>

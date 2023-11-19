b82d25b6d3f55f5c97f9581de143adbe8a36cc7d
shadow1512 <viacheslav.borisov@gmail.com>
2023-11-19 21:00:19 +0300

    type mistake

--- laravel/app/Console/Commands/profileupdatesnotification.php
+++ laravel/app/Console/Commands/profileupdatesnotification.php
@@ -1,4 +1,4 @@
-pro<?php
+<?php
 
 namespace App\Console\Commands;
 
@@ -6,7 +6,6 @@ use Illuminate\Console\Command;
 use App\User;
 use App\Dep;
 use App\Profiles_Saved;
-use App\Profiles_Saved_Data;
 use Mail;
 use Illuminate\Support\Facades\Log;
 use Config;

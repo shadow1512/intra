20b5ed84ec2ccbff99efacc53b556b2dd8474cf6
shadow1512 <viacheslav.borisov@gmail.com>
2023-12-24 22:26:05 +0300

    fix date conversion

--- laravel/app/Http/Controllers/ModerateController.php
+++ laravel/app/Http/Controllers/ModerateController.php
@@ -339,7 +339,7 @@ class ModerateController extends Controller
                 
                 if($i==0) {
                     var_dump(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($dataArray[4]["A"]));
-                    $date = strtotime("Y-m-d", \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($dataArray[4]["A"]));
+                    $date = date("Y-m-d", \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($dataArray[4]["A"]));
                     var_dump($date);
                 }
             }

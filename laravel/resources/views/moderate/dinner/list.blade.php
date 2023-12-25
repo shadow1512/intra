b3e37eec2ad8a97658dc610120e565871b02ccd6
shadow1512 <viacheslav.borisov@gmail.com>
2023-12-25 18:46:10 +0300

    remove slots, remove cameras only menu

--- laravel/resources/views/moderate/dinner/list.blade.php
+++ laravel/resources/views/moderate/dinner/list.blade.php
@@ -29,22 +29,37 @@
         </div>
     </div>
     <div class="row">
-        <form action="{{ route('moderate.dinner.uploadmenu') }}" method="POST" id="menu_fileupload" enctype="multipart/form-data">
-            {{ csrf_field() }}
-            <div class="custom-file"{{ $errors->has('menu_file') ? ' has-error' : '' }}>
-                <input type="file" class="custom-file-input" id="menu_file" name="menu_file" aria-describedby="fileHelpInline">
-                <label class="custom-file-label" for="menu_file">Выберите файл</label>
-                <small id="fileHelpInline" class="text-muted">Файл xlsx с меню на неделю</small>
-            </div>
+        <div class="col-md-8 col-md-offset-2">
 
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
-                        Загрузить
-                    </button>
+            <div class="row">
+                <div class="col-md-9"><h3>Столовая. Загрузка файла с меню</h3></div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="row">
+                <div class="col-md-9">
+                    <form action="{{ route('moderate.dinner.uploadmenu') }}" method="POST" id="menu_fileupload" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <div class="custom-file"{{ $errors->has('menu_file') ? ' has-error' : '' }}>
+                            <input type="file" class="custom-file-input" id="menu_file" name="menu_file" aria-describedby="fileHelpInline">
+                            <label class="custom-file-label" for="menu_file">Выберите файл</label>
+                            <small id="fileHelpInline" class="text-muted">Файл xlsx с меню на неделю</small>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Загрузить
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </form>
+        </div>
     </div>
 </div>
 @endsection

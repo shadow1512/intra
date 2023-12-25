dc5bde1b70a9e957955e96e8d0698f14325a83ad
shadow1512 <viacheslav.borisov@gmail.com>
2023-12-25 17:24:32 +0300

    full path for the file

--- laravel/app/Http/Controllers/ModerateController.php
+++ laravel/app/Http/Controllers/ModerateController.php
@@ -318,10 +318,9 @@ class ModerateController extends Controller
     }
     
     public function dinneruploadmenu(Request $request) {
-        var_dump($request->file('menu_file'));
         if(!is_null($request->file('menu_file'))) {
             $path = Storage::disk('public')->putFileAs(Config::get('dinner.menu_converted_path'), $request->file('menu_file'), Config::get('dinner.filename'), 'public');
-            $this->dinnermenuupdate($path);
+            $this->dinnermenuupdate(storage_path('app/public') . '/' . $path);
         }
     }
     public function dinnermenuupdate($filename) {

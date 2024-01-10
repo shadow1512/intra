4431126ff25fdc32139885a27570bc9adc13ba92
shadow1512 <viacheslav.borisov@gmail.com>
2024-01-10 11:02:13 +0300

    dinner cams show again

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -570,6 +570,6 @@ function openIpModal(button, window) {
 
 openIpModal('.__js-open-ip-modal', '.__js-ip-modal');
 
-//var player1 = new Playerjs({id:"rtmp_cam1", file:"//cam-intra.kodeks.ru:8081/hls1/stream.m3u8"});
-//var player2 = new Playerjs({id:"rtmp_cam2", file:"//cam-intra.kodeks.ru:8081/hls2/stream.m3u8"});
-//var player3 = new Playerjs({id:"rtmp_cam3", file:"//cam-intra.kodeks.ru:8081/hls3/stream.m3u8"});
+var player1 = new Playerjs({id:"rtmp_cam1", file:"//cam-intra.kodeks.ru:8081/hls1/stream.m3u8"});
+var player2 = new Playerjs({id:"rtmp_cam2", file:"//cam-intra.kodeks.ru:8081/hls2/stream.m3u8"});
+var player3 = new Playerjs({id:"rtmp_cam3", file:"//cam-intra.kodeks.ru:8081/hls3/stream.m3u8"});

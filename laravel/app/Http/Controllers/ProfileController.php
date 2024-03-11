5241d6eef62c35dba3dbc8f7f63e04022b1de679
shadow1512 <viacheslav.borisov@gmail.com>
2024-03-11 14:32:34 +0300

    crazy case

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -85,6 +85,7 @@ echo 'c';
         var_dump($sql);
         var_dump($bindings);
         echo $changes->count();
+        var_dump($changes);
         foreach($changes as $item) {
             echo $item->id . "<br>";
             $change_records[$item->id]  =   Profiles_Saved_Data::withTrashed()->where('ps_id',  '=',    $item->id)->get();

b84f53d30069b45179f8d20fa17bb77700057dc3
shadow1512 <viacheslav.borisov@gmail.com>
2025-04-27 16:27:25 +0300

    X instead of x

--- laravel/app/Console/Commands/uploadparseclog.php
+++ laravel/app/Console/Commands/uploadparseclog.php
@@ -63,7 +63,7 @@ class uploadparseclog extends Command
         else {
             Log::error('Parsec: impossible to read XML Parsec file from mnt');
         }*/
-        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("xlsx");
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
         $spreadsheet = $reader->load(Config::get('parsec.parsec_converted_path') . '/'   .   Config::get('parsec.filename'));
         if(!$spreadsheet->getSheetCount()) {
             Log::error('Parsec: no worksheets to parse');

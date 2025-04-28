189ee352919ea911bb180a8db4b940ccb555a190
shadow1512 <viacheslav.borisov@gmail.com>
2025-04-28 11:30:10 +0300

    correct processing order

--- laravel/app/Console/Commands/updateuserpresence.php
+++ laravel/app/Console/Commands/updateuserpresence.php
@@ -40,7 +40,7 @@ class updateuserpresence extends Command
     public function handle()
     {
         //берем только те записи, которые не обработаны
-        $records_to_process    =   Parsec_log::where("processed", "=",  0)->get();
+        $records_to_process    =   Parsec_log::where("processed", "=",  0)->orderBy("datetime_record", "desc")->get();
         foreach($records_to_process as $record) {
             $user_names =   explode(" ",    $record->user);
 

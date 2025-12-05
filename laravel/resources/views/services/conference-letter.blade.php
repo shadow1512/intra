ead80eb1de906187494f785466f3fe415135d259
shadow1512 <viacheslav.borisov@gmail.com>
2025-12-05 20:12:30 +0300

    лишнюю переменную убрали

--- laravel/resources/views/services/conference-letter.blade.php
+++ laravel/resources/views/services/conference-letter.blade.php
@@ -8,4 +8,3 @@
 <p><strong>Площадка проведения вебинара</strong>:{{$provider}}</p>
 <p><strong>Тип вебинара</strong>:{{$typeevent}}</p>
 <p><strong>Помощь в модерации</strong>:{{$moderate}}</p>
-<!--<p><strong>Способ презентации</strong>:{{$presentation}}</p>-->

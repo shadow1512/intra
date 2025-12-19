14098b472829a795f3095adba383f83fb6c6518b
shadow1512 <viacheslav.borisov@gmail.com>
2025-12-19 17:12:45 +0300

    select correct phone

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -255,17 +255,17 @@ class UserController extends Controller
             return abort(403);
         }
             
-        if(is_null($abonent->ip_phone) && is_null($abonent->phone)) {
+        if(!($abonent->ip_phone) && !($abonent->phone)) {
             return abort(403);
         }
         
         $from_phone =   Auth::user()->ip_phone;
-        if(is_null($from_phone)) {
+        if(!($from_phone)) {
             $from_phone = Auth::user()->phone;  
         }
         
         $to_phone =   $abonent->ip_phone;
-        if(is_null($to_phone)) {
+        if(!($to_phone)) {
             $to_phone = $abonent->phone;
         }
         

ec1ef69a6612e0f09000c9f1179b162e96d03879
shadow1512 <viacheslav.borisov@gmail.com>
2025-12-22 21:43:02 +0300

    navigation contacts

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -15,11 +15,19 @@
                     @foreach($contacts as $item)
                 <li class="main_top_phones_lst_i">
                     <span class="main_top_phones_lst_i_lk"><a href="{{route('people.unit', ["id" =>  $item->id])}}" title="{{$item->lname}} {{$item->fname}} {{$item->mname}}">{{$item->lname}} {{mb_substr($item->fname,   0,  1)  .   "."}} {{mb_substr($item->mname, 0,  1)  .   "."}}</a>
-                    ( @if($item->ip_phone) 
-                        @if(Auth::check() && !is_null(Auth::user()->ip_phone)) <a href="{{route("people.call", ["id"   =>  $item->id])}}" class="__js-open-ip-modal">{{$item->ip_phone}}</a> @else {{$item->ip_phone}} @endif 
-                        @if($item->phone) или {{$item->phone}} @endif 
-                      @else {{$item->phone}} 
-                      @endif )
+                    ( @if(Auth::user()->ip_phone || Auth::user()->phone)
+                        @if($item->ip_phone)<a href="{{route("people.call", ["id"   =>  $item->id])}}" class="__js-open-ip-modal">{{$item->ip_phone}}</a>
+                            @if($item->phone) или <a href="{{route("people.call", ["id"   =>  $item->id])}}" class="__js-open-ip-modal">{{$item->phone}}</a>@endif
+                        @else
+                            @if($item->phone)<a href="{{route("people.call", ["id"   =>  $item->id])}}" class="__js-open-ip-modal">{{$item->phone}}</a>@endif 
+                        @endif
+                    @else
+                        @if($item->ip_phone){{$item->ip_phone}}
+                            @if($item->phone) или {{$item->phone}}@endif
+                        @else
+                            @if($item->phone){{$item->phone}}@endif 
+                        @endif
+                    @endif )
                     </span>
                 </li>
                     @endforeach
